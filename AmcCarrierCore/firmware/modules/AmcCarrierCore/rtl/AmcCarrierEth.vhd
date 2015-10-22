13f471d0a0d6a8284d5f80ce433737280efdad55
ruckman <ruckman@slac.stanford.edu>
2015-10-22 04:13:19 +0000

    Restoring the Revision: 8191
    ######################################################################
    
    Please DON’T overwrite someone else commits with your obsolete working copy.  
    
    ######################################################################
    
    git-svn-id: file:///dev/shm/git-devel/svn-repo/LCLS_II/trunk@8198 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 0516a2cb0dee3ec29237d5565bb1a1a975a739eb

---
+++ AmcCarrierCore/firmware/modules/AmcCarrierCore/rtl/AmcCarrierEth.vhd
@@ -0,0 +1,261 @@
+-------------------------------------------------------------------------------
+-- Title      : 
+-------------------------------------------------------------------------------
+-- File       : AmcCarrierEth.vhd
+-- Author     : Larry Ruckman  <ruckman@slac.stanford.edu>
+-- Company    : SLAC National Accelerator Laboratory
+-- Created    : 2015-09-21
+-- Last update: 2015-10-15
+-- Platform   : 
+-- Standard   : VHDL'93/02
+-------------------------------------------------------------------------------
+-- Description: 
+-------------------------------------------------------------------------------
+-- Copyright (c) 2015 SLAC National Accelerator Laboratory
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+use work.StdRtlPkg.all;
+use work.AxiLitePkg.all;
+use work.AxiStreamPkg.all;
+use work.SsiPkg.all;
+use work.UdpEnginePkg.all;
+use work.IpV4EnginePkg.all;
+use work.AmcCarrierPkg.all;
+
+entity AmcCarrierEth is
+   generic (
+      TPD_G             : time            := 1 ns;
+      FFB_CLIENT_SIZE_G : positive        := 1;
+      AXI_ERROR_RESP_G  : slv(1 downto 0) := AXI_RESP_DECERR_C);
+   port (
+      -- Local Configuration
+      localMac          : in  slv(47 downto 0);  --  big-Endian configuration
+      localIp           : in  slv(31 downto 0);  --  big-Endian configuration   
+      -- Master AXI-Lite Interface
+      mAxilReadMasters  : out AxiLiteReadMasterArray(3 downto 0);
+      mAxilReadSlaves   : in  AxiLiteReadSlaveArray(3 downto 0);
+      mAxilWriteMasters : out AxiLiteWriteMasterArray(3 downto 0);
+      mAxilWriteSlaves  : in  AxiLiteWriteSlaveArray(3 downto 0);
+      -- AXI-Lite Interface
+      axilClk           : in  sl;
+      axilRst           : in  sl;
+      axilReadMaster    : in  AxiLiteReadMasterType;
+      axilReadSlave     : out AxiLiteReadSlaveType;
+      axilWriteMaster   : in  AxiLiteWriteMasterType;
+      axilWriteSlave    : out AxiLiteWriteSlaveType;
+      -- BSA Ethernet Interface
+      obBsaMaster       : in  AxiStreamMasterType;
+      obBsaSlave        : out AxiStreamSlaveType;
+      ibBsaMaster       : out AxiStreamMasterType;
+      ibBsaSlave        : in  AxiStreamSlaveType;
+      -- FFB Outbound Interface
+      ffbObMaster       : in  AxiStreamMasterType;
+      ffbObSlave        : out AxiStreamSlaveType;
+      ----------------------
+      -- Top Level Interface
+      ----------------------
+      -- FFB Inbound Interface (ffbClk domain)
+      ffbClk            : in  sl;
+      ffbRst            : in  sl;
+      ffbBus            : out FfbBusType;
+      ----------------
+      -- Core Ports --
+      ----------------   
+      -- XAUI Ports
+      xauiRxP           : in  slv(3 downto 0);
+      xauiRxN           : in  slv(3 downto 0);
+      xauiTxP           : out slv(3 downto 0);
+      xauiTxN           : out slv(3 downto 0);
+      xauiClkP          : in  sl;
+      xauiClkN          : in  sl);
+end AmcCarrierEth;
+
+architecture mapping of AmcCarrierEth is
+
+   signal dmaIbMaster : AxiStreamMasterType;
+   signal dmaIbSlave  : AxiStreamSlaveType;
+   signal dmaObMaster : AxiStreamMasterType;
+   signal dmaObSlave  : AxiStreamSlaveType;
+   signal obMaster    : AxiStreamMasterType;
+
+   signal ibMacMaster : AxiStreamMasterType;
+   signal ibMacSlave  : AxiStreamSlaveType;
+   signal obMacMaster : AxiStreamMasterType;
+   signal obMacSlave  : AxiStreamSlaveType;
+
+begin
+
+   ----------------------
+   -- 10 GigE XAUI Module
+   ----------------------
+   U_Xaui : entity work.XauiGthUltraScaleWrapper
+      generic map (
+         TPD_G            => TPD_G,
+         -- XAUI Configurations
+         XAUI_20GIGE_G    => false,
+         REF_CLK_FREQ_G   => 156.25E+6,
+         -- AXI-Lite Configurations
+         AXI_ERROR_RESP_G => AXI_RESP_SLVERR_C,
+         -- AXI Streaming Configurations
+         AXIS_CONFIG_G    => ssiAxiStreamConfig(8))
+      port map (
+         -- Local Configurations
+         localMac           => localMac,
+         -- Streaming DMA Interface 
+         dmaClk             => axilClk,
+         dmaRst             => axilRst,
+         dmaIbMaster        => dmaIbMaster,
+         dmaIbSlave         => dmaIbSlave,
+         dmaObMaster        => dmaObMaster,
+         dmaObSlave         => dmaObSlave,
+         -- Slave AXI-Lite Interface 
+         axiLiteClk         => axilClk,
+         axiLiteRst         => axilRst,
+         axiLiteReadMaster  => AXI_LITE_READ_MASTER_INIT_C,
+         axiLiteReadSlave   => open,
+         axiLiteWriteMaster => AXI_LITE_WRITE_MASTER_INIT_C,
+         axiLiteWriteSlave  => open,
+         -- Misc. Signals
+         extRst             => axilRst,
+         phyClk             => open,
+         phyRst             => open,
+         phyReady           => open,
+         -- MGT Clock Port (156.25 MHz)
+         gtClkP             => xauiClkP,
+         gtClkN             => xauiClkN,
+         -- MGT Ports
+         gtTxP              => xauiTxP,
+         gtTxN              => xauiTxN,
+         gtRxP              => xauiRxP,
+         gtRxN              => xauiRxN);
+
+   ----------------------------------------         
+   -- Retrofitting the "old" MAC
+   -- to work with new IPv4 and UDP engines
+   ----------------------------------------         
+   SsiInsertSof_Inst : entity work.SsiInsertSof
+      generic map (
+         TPD_G               => TPD_G,
+         COMMON_CLK_G        => true,
+         SLAVE_FIFO_G        => true,
+         MASTER_FIFO_G       => true,
+         SLAVE_AXI_CONFIG_G  => ssiAxiStreamConfig(8),
+         MASTER_AXI_CONFIG_G => IP_ENGINE_CONFIG_C)
+      port map (
+         -- Slave Port
+         sAxisClk    => axilClk,
+         sAxisRst    => axilRst,
+         sAxisMaster => dmaIbMaster,
+         sAxisSlave  => dmaIbSlave,
+         -- Master Port
+         mAxisClk    => axilClk,
+         mAxisRst    => axilRst,
+         mAxisMaster => obMacMaster,
+         mAxisSlave  => obMacSlave);
+
+   FIFO_Inbound : entity work.AxiStreamFifo
+      generic map (
+         -- General Configurations
+         TPD_G               => TPD_G,
+         PIPE_STAGES_G       => 0,
+         SLAVE_READY_EN_G    => true,
+         VALID_THOLD_G       => 1,
+         -- FIFO configurations
+         BRAM_EN_G           => false,
+         USE_BUILT_IN_G      => false,
+         GEN_SYNC_FIFO_G     => true,
+         CASCADE_SIZE_G      => 1,
+         FIFO_ADDR_WIDTH_G   => 4,
+         -- AXI Stream Port Configurations
+         SLAVE_AXI_CONFIG_G  => IP_ENGINE_CONFIG_C,
+         MASTER_AXI_CONFIG_G => ssiAxiStreamConfig(8))
+      port map (
+         -- Slave Port
+         sAxisClk    => axilClk,
+         sAxisRst    => axilRst,
+         sAxisMaster => ibMacMaster,
+         sAxisSlave  => ibMacSlave,
+         -- Master Port
+         mAxisClk    => axilClk,
+         mAxisRst    => axilRst,
+         mAxisMaster => obMaster,
+         mAxisSlave  => dmaObSlave);
+
+   -- Current MAC only support 64-bit inbound transfers
+   dmaObMaster.tValid <= obMaster.tValid;
+   dmaObMaster.tData  <= obMaster.tData;
+   dmaObMaster.tStrb  <= obMaster.tStrb;
+   dmaObMaster.tKeep  <= x"00FF";
+   dmaObMaster.tLast  <= obMaster.tLast;
+   dmaObMaster.tDest  <= obMaster.tDest;
+   dmaObMaster.tId    <= obMaster.tId;
+   dmaObMaster.tUser  <= obMaster.tUser;
+
+   --------------------
+   -- No VLAN Interface
+   --------------------
+   U_NoVlan : entity work.AmcCarrierEthNoVlan
+      generic map (
+         TPD_G => TPD_G)
+      port map (
+         -- Clock and Reset   
+         axilClk           => axilClk,
+         axilRst           => axilRst,
+         -- Local Configuration
+         localMac          => localMac,
+         localIp           => localIp,
+         -- Interface to Ethernet Media Access Controller (MAC)
+         obMacMaster       => obMacMaster,
+         obMacSlave        => obMacSlave,
+         ibMacMaster       => ibMacMaster,
+         ibMacSlave        => ibMacSlave,
+         -- Master AXI-Lite Interface
+         mAxilReadMasters  => mAxilReadMasters,
+         mAxilReadSlaves   => mAxilReadSlaves,
+         mAxilWriteMasters => mAxilWriteMasters,
+         mAxilWriteSlaves  => mAxilWriteSlaves,
+         -- BSA Ethernet Interface
+         obBsaMaster       => obBsaMaster,
+         obBsaSlave        => obBsaSlave,
+         ibBsaMaster       => ibBsaMaster,
+         ibBsaSlave        => ibBsaSlave);
+
+   -----------------
+   -- VLAN Interface
+   -----------------   
+   U_Vlan : entity work.AmcCarrierEthVlan
+      generic map (
+         TPD_G             => TPD_G,
+         FFB_CLIENT_SIZE_G => FFB_CLIENT_SIZE_G,
+         AXI_ERROR_RESP_G  => AXI_ERROR_RESP_G)
+      port map (
+         -- AXI-Lite Interface
+         axilClk         => axilClk,
+         axilRst         => axilRst,
+         axilReadMaster  => axilReadMaster,
+         axilReadSlave   => axilReadSlave,
+         axilWriteMaster => axilWriteMaster,
+         axilWriteSlave  => axilWriteSlave,
+         -- Local Configuration
+         localMac        => localMac,
+         localIp         => localIp,
+         -- Interface to Ethernet Media Access Controller (MAC)
+         obMacMaster     => AXI_STREAM_MASTER_INIT_C,
+         obMacSlave      => open,
+         ibMacMaster     => open,
+         ibMacSlave      => AXI_STREAM_SLAVE_FORCE_C,
+         -- FFB Outbound Interface
+         ffbObMaster     => ffbObMaster,
+         ffbObSlave      => ffbObSlave,
+         ----------------------
+         -- Top Level Interface
+         ----------------------
+         -- FFB Inbound Interface (ffbClk domain)
+         ffbClk          => ffbClk,
+         ffbRst          => ffbRst,
+         ffbBus          => ffbBus);
+
+end mapping;
---
+++ AmcCarrierCore/firmware/modules/AmcCarrierCore/rtl/AmcCarrierRegPkg.vhd
@@ -0,0 +1,97 @@
+-------------------------------------------------------------------------------
+-- Title      : 
+-------------------------------------------------------------------------------
+-- File       : AmcCarrierRegPkg.vhd
+-- Author     : Larry Ruckman  <ruckman@slac.stanford.edu>
+-- Company    : SLAC National Accelerator Laboratory
+-- Created    : 2015-09-08
+-- Last update: 2015-10-02
+-- Platform   : 
+-- Standard   : VHDL'93/02
+-------------------------------------------------------------------------------
+-- Description: 
+-------------------------------------------------------------------------------
+-- Copyright (c) 2015 SLAC National Accelerator Laboratory
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+use work.StdRtlPkg.all;
+use work.AmcCarrierPkg.all;
+
+package AmcCarrierRegPkg is
+
+   constant AXI_CLK_FREQ_C   : real := 156.25E+6;                        -- In units of Hz
+   constant AXI_CLK_PERIOD_C : real := getRealDiv(1.0, AXI_CLK_FREQ_C);  -- In units of seconds
+
+   ---------------------------------------------
+   -- Register Mapping: 1st Layer base addresses
+   ---------------------------------------------
+   constant VERSION_ADDR_C    : slv(31 downto 0) := x"00000000";
+   constant SYSMON_ADDR_C     : slv(31 downto 0) := x"01000000";
+   constant BOOT_MEM_ADDR_C   : slv(31 downto 0) := x"02000000";
+   constant XBAR_ADDR_C       : slv(31 downto 0) := x"03000000";
+   constant CONFIG_I2C_ADDR_C : slv(31 downto 0) := x"04000000";
+   constant CLK_I2C_ADDR_C    : slv(31 downto 0) := x"05000000";
+   constant DDR_I2C_ADDR_C    : slv(31 downto 0) := x"06000000";
+   constant IPMC_ADDR_C       : slv(31 downto 0) := x"07000000";
+   constant TIMING_ADDR_C     : slv(31 downto 0) := x"08000000";
+   constant BSA_ADDR_C        : slv(31 downto 0) := X"09000000";
+   constant XAUI_ADDR_C       : slv(31 downto 0) := x"0A000000";
+   constant DDR_ADDR_C        : slv(31 downto 0) := x"0B000000";
+   constant MPS_ADDR_C        : slv(31 downto 0) := x"0C000000";
+   constant APP_ADDR_C        : slv(31 downto 0) := x"80000000";
+   
+   constant XBAR_TIME_GEN_C : Slv2Array(3 downto 0) := (
+      3 => "01",                        -- OUT[3] = IN[1], DIST1 = FPGA
+      2 => "01",                        -- OUT[2] = IN[1], DIST0 = FPGA
+      1 => "01",                        -- OUT[1] = IN[1], FPGA  = FPGA (loopback)
+      0 => "01");                       -- OUT[0] = IN[1], RTM0  = FPGA
+
+   constant XBAR_MPS_II_LINK_C : Slv2Array(3 downto 0) := (
+      3 => "11",                        -- OUT[3] = IN[3], DIST1 = RTM1 (LCLS-II)
+      2 => "11",                        -- OUT[2] = IN[3], DIST0 = RTM1 (LCLS-II)
+      1 => "11",                        -- OUT[1] = IN[3], FPGA  = RTM1 (LCLS-II)
+      0 => "00");                       -- OUT[0] = IN[0], RTM0  = RTM0 (loopback)
+
+   constant XBAR_MPS_I_LINK_C : Slv2Array(3 downto 0) := (
+      3 => "00",                        -- OUT[3] = IN[0], DIST1 = RTM0 (LCLS-I)
+      2 => "00",                        -- OUT[2] = IN[0], DIST0 = RTM0 (LCLS-I)
+      1 => "00",                        -- OUT[1] = IN[0], FPGA  = RTM0 (LCLS-I)
+      0 => "00");                       -- OUT[0] = IN[0], RTM0  = RTM0 (loopback)      
+
+   constant XBAR_APP_NODE_C : Slv2Array(3 downto 0) := (
+      3 => "01",                        -- OUT[3] = IN[1], DIST1 = FPGA
+      2 => "01",                        -- OUT[2] = IN[1], DIST0 = FPGA
+      1 => "10",                        -- OUT[1] = IN[2], FPGA  = Backplane timing
+      0 => "00");                       -- OUT[0] = IN[0], RTM0  = RTM0 (loopback)      
+
+   function xbarDefault(appType : AppType; sel : boolean) return Slv2Array;
+
+end package AmcCarrierRegPkg;
+
+package body AmcCarrierRegPkg is
+
+   function xbarDefault (appType : AppType; sel : boolean) return Slv2Array is
+      variable retVar : Slv2Array(3 downto 0);
+   begin
+      case appType is
+         -- Check for Timing Generator Node
+         when APP_TIME_GEN_TYPE_C =>
+            retVar := XBAR_TIME_GEN_C;
+         -- Check for MPS Link Node
+         when APP_MPS_LINK_TYPE_C =>
+            if sel then
+               retVar := XBAR_MPS_I_LINK_C;
+            else
+               retVar := XBAR_MPS_II_LINK_C;
+            end if;
+         -- Else Application Node
+         when others =>
+            retVar := XBAR_APP_NODE_C;
+      end case;
+      return retVar;
+   end function;
+
+end package body AmcCarrierRegPkg;
