c9fa870fb28df4929557c998529ae227ae84aa4b
ruckman <ruckman@slac.stanford.edu>
2016-02-26 18:15:36 +0000

    Added another register delay stage to help with timing via register replication
    git-svn-id: file:///dev/shm/git-devel/svn-repo/LCLS_II/trunk@9466 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 34d5f70263e456bbfb9975c4ffb97859428b6f65

--- AmcCarrierCore/firmware/modules/AmcCarrierCore/AmcCarrier/AmcCarrierDdrMem.vhd
+++ AmcCarrierCore/firmware/modules/AmcCarrierCore/AmcCarrier/AmcCarrierDdrMem.vhd
@@ -5,7 +5,7 @@
 -- Author     : Larry Ruckman  <ruckman@slac.stanford.edu>
 -- Company    : SLAC National Accelerator Laboratory
 -- Created    : 2015-07-08
--- Last update: 2016-02-23
+-- Last update: 2016-02-25
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -166,8 +166,7 @@ architecture mapping of AmcCarrierDdrMem is
    signal done       : sl;
    signal refClock   : sl;
    signal refClkBufg : sl;
-   signal coreRst    : sl;
-   signal coreRstDly : sl;
+   signal coreRst    : slv(2 downto 0);
 
    attribute KEEP_HIERARCHY                : string;
    attribute KEEP_HIERARCHY of IBUFDS_Inst : label is "TRUE";
@@ -245,7 +244,7 @@ begin
          c0_ddr3_reset_n         => ddrRstL,
          c0_ddr3_we_n            => ddrWeL,
          c0_ddr3_ui_clk          => ddrClk,
-         c0_ddr3_ui_clk_sync_rst => coreRst,
+         c0_ddr3_ui_clk_sync_rst => coreRst(0),
          c0_ddr3_aresetn         => axiRstL,
          c0_ddr3_s_axi_awid      => ddrWriteMaster.awid(3 downto 0),
          c0_ddr3_s_axi_awaddr    => ddrWriteMaster.awaddr(32 downto 0),
@@ -289,8 +288,9 @@ begin
    process(ddrClk)
    begin
       if rising_edge(ddrClk) then
-         coreRstDly <= coreRst    after TPD_G;  -- Register to help with timing
-         ddrRst     <= coreRstDly after TPD_G;  -- Register to help with timing
+         coreRst(1) <= coreRst(0) after TPD_G;  -- Register to help with timing
+         coreRst(2) <= coreRst(1) after TPD_G;  -- Register to help with timing
+         ddrRst     <= coreRst(2) after TPD_G;  -- Register to help with timing
       end if;
    end process;
 
