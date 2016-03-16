911cb731f70029a16f33bdad4d81cab999942ad4
bareese <bareese@slac.stanford.edu>
2016-03-16 22:45:21 +0000

    Add BsaAxiInterconnect DCP.
    git-svn-id: file:///dev/shm/git-devel/svn-repo/LCLS_II/trunk@9744 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: e72540f2f6589ee9322e9bdd7c677a2f14c49651

--- AmcCarrierCore/firmware/modules/AmcCarrierCore/vivado/debug_properties.tcl
+++ AmcCarrierCore/firmware/modules/AmcCarrierCore/vivado/debug_properties.tcl
@@ -27,3 +27,5 @@ set_property SCOPED_TO_CELLS {inst/u_ddr3_mem_intfc/u_ddr_cal_riu/mcs0/microblaz
 add_files ${PROJ_DIR}/../../modules/AmcCarrierCore/coregen/DebugMigCoreMicroblazeCalibration.bmm
 set_property SCOPED_TO_REF   {DebugMigCore} [get_files DebugMigCoreMicroblazeCalibration.bmm]
 set_property SCOPED_TO_CELLS {inst/u_ddr3_mem_intfc/u_ddr_cal_riu/mcs0} [get_files DebugMigCoreMicroblazeCalibration.bmm]
+
+add_files -quiet -fileset sources_1 ${PROJ_DIR}/../../modules/BsaCore/cores/BsaAxiInterconnect/xilinxUltraScale/BsaAxiInterconnect.dcp
