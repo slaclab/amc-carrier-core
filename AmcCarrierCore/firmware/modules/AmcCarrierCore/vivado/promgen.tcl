746bd9736b1e5825a604c6040dee551fbc4169cc
ruckman <ruckman@slac.stanford.edu>
2016-08-10 19:03:23 +0000

    1) Added XCKU060 framework to the firmware build 2) Added the XCKU060 version of FSBL and EMPTY firmware targets
    git-svn-id: file:///dev/shm/git-devel/svn-repo/LCLS_II/trunk@11816 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: a59042dcdf810b036953a1d1ba6c8575bbd55928

--- AmcCarrierCore/firmware/modules/AmcCarrierCore/vivado/promgen.tcl
+++ AmcCarrierCore/firmware/modules/AmcCarrierCore/vivado/promgen.tcl
@@ -7,19 +7,30 @@
 ## may be copied, modified, propagated, or distributed except according to 
 ## the terms contained in the LICENSE.txt file.
 ##############################################################################
+
+# PROM Configurations
 set format     "mcs"
 set inteface   "SPIx1"
 set size       "1024"
 
+# XCKU040/XCKU060 .BIT file locations
 set FSBL_BIT    "0x00000000"
 set LCLS_I_BIT  "0x02000000"
 set LCLS_II_BIT "0x04000000"
 set TEMP_BIT    "0x06000000"
 
+# XCKU040 .TAR.GZ file locations (up to 17 MB file)
 set FSBL_GZ    "0x00F43EFC"
 set LCLS_I_GZ  "0x02F43EFC"
 set LCLS_II_GZ "0x04F43EFC"
 set TEMP_GZ    "0x06F43EFC"
 
+# XCKU060 .TAR.GZ file locations (up to 9 MB file)
+set FSBL_GZ_KU060    "0x01701DEC"
+set LCLS_I_GZ_KU060  "0x03701DEC"
+set LCLS_II_GZ_KU060 "0x05701DEC"
+set TEMP_GZ_KU060    "0x07701DEC"
+
+# File Paths
 set BIT_PATH   "$::env(IMPL_DIR)/$::env(PROJECT).bit"
 set DATA_PATH  "$::env(IMAGES_DIR)/$::env(PROJECT)_$::env(PRJ_VERSION).tar.gz"
