c59f1022f0072273b255c4460fef4b6ac8f29d5d
ruckman <ruckman@slac.stanford.edu>
2016-09-06 18:06:07 +0000

    Added SLAC licensing.
    git-svn-id: file:///afs/slac/g/reseng/svn/repos/LCLS_II/trunk@12270 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: 247ff69618cff22b5e606d0f8ff290a863b4477b

--- BsaCore/rtl/SsiAxiMaster.vhd
+++ BsaCore/rtl/SsiAxiMaster.vhd
@@ -12,7 +12,13 @@
 -- Description:
 -- Block for Register protocol.
 -------------------------------------------------------------------------------
--- Copyright (c) 2014 by Ryan Herbst. All rights reserved.
+-- This file is part of 'LCLS2 Timing BSA Core'.
+-- It is subject to the license terms in the LICENSE.txt file found in the 
+-- top-level directory of this distribution and at: 
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+-- No part of 'LCLS2 Timing BSA Core', including this file, 
+-- may be copied, modified, propagated, or distributed except according to 
+-- the terms contained in the LICENSE.txt file.
 -------------------------------------------------------------------------------
 -- Modification history:
 -- 04/09/2014: created.
