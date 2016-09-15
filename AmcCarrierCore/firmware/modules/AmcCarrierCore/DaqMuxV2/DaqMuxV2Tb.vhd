5f2998f879e8f3d166ead4e7eb333845e45ea8d2
ulegat <ulegat@slac.stanford.edu>
2016-09-15 19:32:22 +0000

    Updating to new YAML schema
    git-svn-id: file:///afs/slac/g/reseng/svn/repos/LCLS_II/trunk@12484 0848cbd7-7569-0410-ab05-8cc0a0c67708
    
    Former-commit-id: d175e0bb4545b704ebfe6539e78ed852b351c7bd

--- AmcCarrierCore/firmware/modules/AmcCarrierCore/DaqMuxV2/DaqMuxV2Tb.vhd
+++ AmcCarrierCore/firmware/modules/AmcCarrierCore/DaqMuxV2/DaqMuxV2Tb.vhd
@@ -1,12 +1,3 @@
-------------------------------------------------------------------------------
--- This file is part of 'LCLS2 Common Carrier Core'.
--- It is subject to the license terms in the LICENSE.txt file found in the 
--- top-level directory of this distribution and at: 
---    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
--- No part of 'LCLS2 Common Carrier Core', including this file, 
--- may be copied, modified, propagated, or distributed except according to 
--- the terms contained in the LICENSE.txt file.
-------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
@@ -162,16 +153,16 @@ begin
       wait for CLK_PERIOD_C*20;    
       rxAxisSlaveArr_i(0)  <= AXI_STREAM_SLAVE_FORCE_C;
       
-      wait for CLK_PERIOD_C*1000;
-      trigHw_i <= '1';    
-      wait for CLK_PERIOD_C*20;
-      trigHw_i <= '0';
-      
       wait for CLK_PERIOD_C*100;
       freezeHw_i <= '1';    
       wait for CLK_PERIOD_C*20;
       freezeHw_i <= '0';
-      
+
+      wait for CLK_PERIOD_C*1000;
+      trigHw_i <= '1';    
+      wait for CLK_PERIOD_C*20;
+      trigHw_i <= '0';
+           
       wait for CLK_PERIOD_C*5000;
       trigCasc_i <= '1';    
       wait for CLK_PERIOD_C*20;
