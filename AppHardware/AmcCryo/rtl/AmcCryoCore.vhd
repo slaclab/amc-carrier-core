be76200d15e91a67d20adfd8e7512251a1b618a1
Larry Ruckman <ruckman@slac.stanford.edu>
2017-05-16 10:30:51 -0700

    cherry picking Uros's master branch changes

---
+++ AppHardware/AmcCryo/rtl/AmcCryoCore.vhd
@@ -0,0 +1,414 @@
+-------------------------------------------------------------------------------
+-- Title      : LCLS-II Cryo-Sensor High Speed ADC/DAC Board
+-------------------------------------------------------------------------------
+-- File       : AmcCryoCore.vhd
+-- Author     : Uros Legat <ulegat@slac.stanford.edu>
+-- Company    : SLAC National Accelerator Laboratory (Cosylab)
+-- Created    : 2017-10-05
+-- Last update: 2017-10-05
+-- Platform   : LCLS2 Common Plaform Carrier
+--              AMC ADC/Analog demo
+-- Standard   : VHDL'93/02
+-------------------------------------------------------------------------------
+-- Description:
+--    
+--    8 lane JESD receiver ADC
+--    8 lane JESD transmitter DAC
+--    SPI: 1 LMK chip, 2 ADC chip (ADC32RF45), and 2 DAC chip () 
+--
+--    https://confluence.slac.stanford.edu/pages/viewpage.action?spaceKey=AIRTRACK&title=PC_379_396_23_C00
+-------------------------------------------------------------------------------
+-- This file is part of 'LCLS2 LLRF Development'.
+-- It is subject to the license terms in the LICENSE.txt file found in the 
+-- top-level directory of this distribution and at: 
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+-- No part of 'LCLS2 LLRF Development', including this file, 
+-- may be copied, modified, propagated, or distributed except according to 
+-- the terms contained in the LICENSE.txt file.
+-------------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.std_logic_arith.all;
+
+library unisim;
+use unisim.vcomponents.all;
+
+use work.StdRtlPkg.all;
+use work.AxiLitePkg.all;
+use work.AxiStreamPkg.all;
+use work.jesd204bPkg.all;
+
+entity AmcCryoCore is
+   generic (
+      TPD_G            : time             := 1 ns;
+      AXI_CLK_FREQ_G   : real             := 156.25E+6;
+      AXI_ERROR_RESP_G : slv(1 downto 0)  := AXI_RESP_DECERR_C;
+      AXI_BASE_ADDR_G  : slv(31 downto 0) := (others => '0'));
+   port (
+      -- Internal portsž
+      
+      -- For resetting ADC Chips on the board
+      adcRst          : in slv(1 downto 0);
+      -- For giving LMK a reference clock from fpga
+      lmkRef          : in sl;            
+      
+      -- JESD Interface
+      jesdSysRef      : out   sl;
+      jesdRxSync      : in    sl;
+      jesdTxSync      : out   sl;
+
+      -- AXI-Lite Interface
+      axilClk         : in    sl;
+      axilRst         : in    sl;
+      axilReadMaster  : in    AxiLiteReadMasterType;
+      axilReadSlave   : out   AxiLiteReadSlaveType;
+      axilWriteMaster : in    AxiLiteWriteMasterType;
+      axilWriteSlave  : out   AxiLiteWriteSlaveType;
+
+      -----------------------
+      -- Application Ports --
+      -----------------------      
+      -- AMC's JTAG Ports
+      jtagPri         : inout slv(4 downto 0);
+      jtagSec         : inout slv(4 downto 0);
+      -- AMC's FPGA Clock Ports
+      fpgaClkP        : inout slv(1 downto 0);
+      fpgaClkN        : inout slv(1 downto 0);
+      -- AMC's System Reference Ports
+      sysRefP         : inout slv(3 downto 0);
+      sysRefN         : inout slv(3 downto 0);
+      -- AMC's Sync Ports
+      syncInP         : inout slv(3 downto 0);
+      syncInN         : inout slv(3 downto 0);
+      syncOutP        : inout slv(9 downto 0);
+      syncOutN        : inout slv(9 downto 0);
+      -- AMC's Spare Ports
+      spareP          : inout slv(15 downto 0);
+      spareN          : inout slv(15 downto 0);
+       -- AMC's IO Ports kcu60 only
+      amcIoP          : inout slv(3 downto 0);
+      amcIoN          : inout slv(3 downto 0)   
+      );
+end AmcCryoCore;
+
+architecture top_level_app of AmcCryoCore is
+   
+   -------------------------------------------------------------------------------------------------
+   -- AXI Lite Config and Signals
+   -------------------------------------------------------------------------------------------------
+   constant NUM_AXI_MASTERS_C : natural := 5;
+
+   constant ADC_0_INDEX_C        : natural := 0;
+   constant ADC_1_INDEX_C        : natural := 1;
+   constant DAC_0_INDEX_C        : natural := 2;
+   constant DAC_1_INDEX_C        : natural := 3;
+   constant LMK_INDEX_C          : natural := 4;
+
+   constant ADC_0_BASE_ADDR_C        : slv(31 downto 0) := X"0002_0000" + AXI_BASE_ADDR_G;
+   constant ADC_1_BASE_ADDR_C        : slv(31 downto 0) := X"0004_0000" + AXI_BASE_ADDR_G;
+   constant DAC_0_BASE_ADDR_C        : slv(31 downto 0) := X"0006_0000" + AXI_BASE_ADDR_G;
+   constant DAC_1_BASE_ADDR_C        : slv(31 downto 0) := X"0008_0000" + AXI_BASE_ADDR_G;
+   constant LMK_BASE_ADDR_C          : slv(31 downto 0) := X"000A_0000" + AXI_BASE_ADDR_G;
+
+   
+   constant AXI_CROSSBAR_MASTERS_CONFIG_C : AxiLiteCrossbarMasterConfigArray(NUM_AXI_MASTERS_C-1 downto 0) := (
+      ADC_0_INDEX_C        => (
+         baseAddr          => ADC_0_BASE_ADDR_C,
+         addrBits          => 17,
+         connectivity      => X"FFFF"),
+      ADC_1_INDEX_C        => (
+         baseAddr          => ADC_1_BASE_ADDR_C,
+         addrBits          => 17,
+         connectivity      => X"FFFF"),
+      DAC_0_INDEX_C        => (
+         baseAddr          => DAC_0_BASE_ADDR_C,
+         addrBits          => 17,
+         connectivity      => X"FFFF"),
+      DAC_1_INDEX_C        => (
+         baseAddr          => DAC_1_BASE_ADDR_C,
+         addrBits          => 17,
+         connectivity      => X"FFFF"),  
+      LMK_INDEX_C          => (
+         baseAddr          => LMK_BASE_ADDR_C,
+         addrBits          => 17,
+         connectivity      => X"FFFF"));
+
+   signal locAxilWriteMasters : AxiLiteWriteMasterArray(NUM_AXI_MASTERS_C-1 downto 0);
+   signal locAxilWriteSlaves  : AxiLiteWriteSlaveArray(NUM_AXI_MASTERS_C-1 downto 0);
+   signal locAxilReadMasters  : AxiLiteReadMasterArray(NUM_AXI_MASTERS_C-1 downto 0);
+   signal locAxilReadSlaves   : AxiLiteReadSlaveArray(NUM_AXI_MASTERS_C-1 downto 0);
+
+   -----------------------
+   -- Application Ports --
+   -----------------------
+
+   -------------------------------------------------------------------------------------------------
+   -- JESD constants and signals
+   -------------------------------------------------------------------------------------------------
+   -- JESD Reference Ports
+   signal jesdSysRefP : sl;
+   signal jesdSysRefN : sl;
+   -- JESD Sync Ports
+   signal jesdRxSyncP : slv(1 downto 0);
+   signal jesdRxSyncN : slv(1 downto 0);
+   signal jesdTxSyncP : slv(1 downto 0);
+   signal jesdTxSyncN : slv(1 downto 0);
+   signal jesdTxSyncVec : slv(1 downto 0);   
+
+   -------------------------------------------------------------------------------------------------
+   -- SPI
+   -------------------------------------------------------------------------------------------------   
+   
+   -- ADC SPI config interface   
+   signal adcCoreClk  : slv(1 downto 0);
+   signal adcCoreDout : slv(1 downto 0);
+   signal adcCoreCsb  : slv(1 downto 0);
+
+   signal adcMuxClk  : sl;
+   signal adcMuxDout : sl;
+   
+   signal adcSpiClk : sl;
+   signal adcSpiDi  : sl;
+   signal adcSpiDo  : sl;
+   signal adcSpiCsb : slv(1 downto 0);
+   
+   -- DAC SPI config interface 
+   signal dacCoreClk  : slv(1 downto 0);
+   signal dacCoreDout : slv(1 downto 0);
+   signal dacCoreCsb  : slv(1 downto 0);
+
+   signal dacMuxClk  : sl;
+   signal dacMuxDout : sl;
+   signal dacMuxDin  : sl;
+   
+   signal dacSpiClk : sl;
+   signal dacSpiDio : sl;
+   signal dacSpiCsb : slv(1 downto 0);
+   
+   
+   -- LMK SPI config interface
+   signal lmkSpiDout : sl;
+   signal lmkSpiDin : sl;
+   
+   signal lmkSpiClk : sl;
+   signal lmkSpiDio : sl;
+   signal lmkSpiCsb : sl;  
+   
+begin
+   -----------------------
+   -- Generalized Mapping 
+   -----------------------
+
+   -- JESD Reference Ports
+   jesdSysRefP <= sysRefP(0);
+   jesdSysRefN <= sysRefN(0);
+
+   -- JESD Sync Ports
+   syncInP(3) <= jesdRxSyncP(0);
+   syncInN(3) <= jesdRxSyncN(0);
+   syncInP(0) <= jesdRxSyncP(1);
+   syncInN(0) <= jesdRxSyncN(1);
+   jesdTxSyncP(0)  <= syncOutP(0);
+   jesdTxSyncN(0)  <= syncOutN(0);  
+   jesdTxSyncP(1)  <= syncOutP(1);
+   jesdTxSyncN(1)  <= syncOutN(1);
+   
+   -- ADC SPI 
+   spareP(2)   <= adcSpiDo;   
+   spareN(1)   <= adcSpiClk;   
+   spareN(2)   <= adcSpiCsb(0);
+   syncOutN(8) <= adcSpiCsb(1); 
+   adcSpiDi    <= syncOutP(9);
+   
+   -- DAC SPI
+   spareP(0) <= dacSpiClk;
+   spareP(1) <= dacSpiDio;   
+   spareN(0)   <= dacSpiCsb(0);
+   syncOutP(8) <= dacSpiCsb(1);
+
+   -- LMK SPI
+   spareP(4) <= lmkSpiClk;
+   spareP(5) <= lmkSpiDio;
+   spareP(3) <= lmkSpiCsb;   
+   
+   -- ADC resets remapping
+   spareN(3)   <= adcRst(0);
+   syncOutN(9) <= adcRst(1);
+   
+   -- Fpga clock reference
+   
+   OBUFDS_lmkRef : OBUFDS
+      port map (
+         I  => lmkRef,
+         O  => amcIoP(2),
+         OB => amcIoN(2));
+   
+   -------------------------------------------------------------------------------------------------
+   -- Application Top Axi Crossbar
+   -------------------------------------------------------------------------------------------------
+   U_XBAR0 : entity work.AxiLiteCrossbar
+      generic map (
+         TPD_G              => TPD_G,
+         DEC_ERROR_RESP_G   => AXI_ERROR_RESP_G,
+         NUM_SLAVE_SLOTS_G  => 1,
+         NUM_MASTER_SLOTS_G => NUM_AXI_MASTERS_C,
+         MASTERS_CONFIG_G   => AXI_CROSSBAR_MASTERS_CONFIG_C)
+      port map (
+         axiClk              => axilClk,
+         axiClkRst           => axilRst,
+         sAxiWriteMasters(0) => axilWriteMaster,
+         sAxiWriteSlaves(0)  => axilWriteSlave,
+         sAxiReadMasters(0)  => axilReadMaster,
+         sAxiReadSlaves(0)   => axilReadSlave,
+         mAxiWriteMasters    => locAxilWriteMasters,
+         mAxiWriteSlaves     => locAxilWriteSlaves,
+         mAxiReadMasters     => locAxilReadMasters,
+         mAxiReadSlaves      => locAxilReadSlaves);
+         
+   ----------------------------------------------------------------
+   -- JESD Buffers
+   ----------------------------------------------------------------
+   IBUFDS_SysRef : IBUFDS
+      port map (
+         I  => jesdSysRefP,
+         IB => jesdSysRefN,
+         O  => jesdSysRef);
+         
+   GEN_RX_SYNC :
+   for i in 1 downto 0 generate
+      OBUFDS_RxSync : OBUFDS
+         port map (
+            I  => jesdRxSync,
+            O  => jesdRxSyncP(i),
+            OB => jesdRxSyncN(i));
+   end generate GEN_RX_SYNC;
+   
+   GEN_TX_SYNC :
+   for i in 1 downto 0 generate
+   IBUFDS_TxSync : IBUFDS
+      port map (
+         I  => jesdTxSyncP(i),
+         IB => jesdTxSyncN(i),
+         O  => jesdTxSyncVec(i));
+   end generate GEN_TX_SYNC;
+   
+   jesdTxSync <= uOr(jesdTxSyncVec); -- Warning!!! Or only at initial debug TODO make it AND
+   -- jesdTxSync <= uAnd(jesdTxSyncVec);   
+   
+   ----------------------------------------------------------------
+   -- SPI interface ADC
+   ----------------------------------------------------------------
+   GEN_ADC : for i in 1 downto 0 generate
+      AxiSpiMaster_INST : entity work.AxiSpiMaster
+         generic map (
+            TPD_G             => TPD_G,
+            ADDRESS_SIZE_G    => 15,
+            DATA_SIZE_G       => 8,
+            CLK_PERIOD_G      => (1.0/AXI_CLK_FREQ_G),
+            SPI_SCLK_PERIOD_G => 10.0E-6)
+         port map (
+            axiClk         => axilClk,
+            axiRst         => axilRst,
+            axiReadMaster  => locAxilReadMasters(ADC_0_INDEX_C+i),
+            axiReadSlave   => locAxilReadSlaves(ADC_0_INDEX_C+i),
+            axiWriteMaster => locAxilWriteMasters(ADC_0_INDEX_C+i),
+            axiWriteSlave  => locAxilWriteSlaves(ADC_0_INDEX_C+i),
+            coreSclk       => adcCoreClk(i),
+            coreSDin       => adcSpiDo,
+            coreSDout      => adcCoreDout(i),
+            coreCsb        => adcCoreCsb(i));
+   end generate GEN_ADC;
+
+   -- Output mux
+   with adcCoreCsb select
+      adcMuxClk <=   adcCoreClk(0) when "10",
+                     adcCoreClk(1) when "01",
+                     '0'           when others;
+   
+   with adcCoreCsb select
+      adcMuxDout <=  adcCoreDout(0) when "10",
+                     adcCoreDout(1) when "01",
+                     '0'            when others;
+   -- IO Assignment
+   adcSpiClk <= adcMuxClk;
+   adcSpiDi  <= adcMuxDout;   
+   adcSpiCsb <= adcCoreCsb;
+   
+      
+   ----------------------------------------------------------------
+   -- SPI interface DAC
+   ----------------------------------------------------------------
+   GEN_DAC : for i in 1 downto 0 generate
+      U_dacAxiSpiMaster : entity work.AxiSpiMaster
+         generic map (
+            TPD_G             => TPD_G,
+            ADDRESS_SIZE_G    => 7,
+            DATA_SIZE_G       => 16,
+            CLK_PERIOD_G      => (1.0/AXI_CLK_FREQ_G),
+            SPI_SCLK_PERIOD_G => 10.0E-6)
+         port map (
+            axiClk         => axilClk,
+            axiRst         => axilRst,
+            axiReadMaster  => locAxilReadMasters(DAC_0_INDEX_C+i),
+            axiReadSlave   => locAxilReadSlaves(DAC_0_INDEX_C+i),
+            axiWriteMaster => locAxilWriteMasters(DAC_0_INDEX_C+i),
+            axiWriteSlave  => locAxilWriteSlaves(DAC_0_INDEX_C+i),
+            coreSclk       => dacCoreClk(i),
+            coreSDin       => dacMuxDin,
+            coreSDout      => dacCoreDout(i),
+            coreCsb        => dacCoreCsb(i));
+   end generate GEN_DAC;
+   
+   -- Output mux
+   with dacCoreCsb select
+      dacMuxClk <= dacCoreClk(0) when "10",
+                   dacCoreClk(1) when "01",
+                   '0'           when others;
+   
+   with dacCoreCsb select
+      dacMuxDout <=  dacCoreDout(0) when "10",
+                     dacCoreDout(1) when "01",
+                     '0'            when others;
+   -- IO Assignment
+   IOBUF_Dac : IOBUF
+      port map (
+         I  => '0',
+         O  => dacMuxDin,
+         IO => dacSpiDio,
+         T  => dacMuxDout);
+         
+   dacSpiClk <= dacMuxClk;     
+   dacSpiCsb <= dacCoreCsb;
+         
+   -----------------
+   -- SPI interface LMK
+   -----------------   
+   U_lmkAxiSpiMaster : entity work.AxiSpiMaster
+      generic map (
+         TPD_G             => TPD_G,
+         AXI_ERROR_RESP_G  => AXI_ERROR_RESP_G,
+         ADDRESS_SIZE_G    => 15,
+         DATA_SIZE_G       => 8,
+         CLK_PERIOD_G      => (1.0/AXI_CLK_FREQ_G),
+         SPI_SCLK_PERIOD_G => 1.0E-6)
+      port map (
+         axiClk         => axilClk,
+         axiRst         => axilRst,
+         axiReadMaster  => locAxilReadMasters(LMK_INDEX_C),
+         axiReadSlave   => locAxilReadSlaves(LMK_INDEX_C),
+         axiWriteMaster => locAxilWriteMasters(LMK_INDEX_C),
+         axiWriteSlave  => locAxilWriteSlaves(LMK_INDEX_C),
+         coreSclk       => lmkSpiClk,
+         coreSDin       => lmkSpiDin,
+         coreSDout      => lmkSpiDout,
+         coreCsb        => lmkSpiCsb);
+
+   IOBUF_Lmk : IOBUF
+      port map (
+         I  => '0',
+         O  => lmkSpiDin,
+         IO => lmkSpiDio,
+         T  => lmkSpiDout);
+
+end top_level_app;
---
+++ AppHardware/AmcCryo/xdc/AmcCryoBay0Pinout.xdc
@@ -0,0 +1,84 @@
+##############################################################################
+## This file is part of 'LCLS2 Common Carrier Core'.
+## It is subject to the license terms in the LICENSE.txt file found in the 
+## top-level directory of this distribution and at: 
+##    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+## No part of 'LCLS2 Common Carrier Core', including this file, 
+## may be copied, modified, propagated, or distributed except according to 
+## the terms contained in the LICENSE.txt file.
+##############################################################################
+# JESD High Speed Ports
+set_property PACKAGE_PIN AH6 [get_ports {jesdTxP[0][0]}] ; #
+set_property PACKAGE_PIN AH5 [get_ports {jesdTxN[0][0]}] ; #
+set_property PACKAGE_PIN AH2 [get_ports {jesdRxP[0][0]}] ; #
+set_property PACKAGE_PIN AH1 [get_ports {jesdRxN[0][0]}] ; #
+set_property PACKAGE_PIN AG4 [get_ports {jesdTxP[0][1]}] ; # 
+set_property PACKAGE_PIN AG3 [get_ports {jesdTxN[0][1]}] ; #
+set_property PACKAGE_PIN AF2 [get_ports {jesdRxP[0][1]}] ; #
+set_property PACKAGE_PIN AF1 [get_ports {jesdRxN[0][1]}] ; #
+set_property PACKAGE_PIN AE4 [get_ports {jesdTxP[0][2]}] ; #
+set_property PACKAGE_PIN AE3 [get_ports {jesdTxN[0][2]}] ; #
+set_property PACKAGE_PIN AD2 [get_ports {jesdRxP[0][2]}] ; #
+set_property PACKAGE_PIN AD1 [get_ports {jesdRxN[0][2]}] ; #
+set_property PACKAGE_PIN AC4 [get_ports {jesdTxP[0][3]}] ; #
+set_property PACKAGE_PIN AC3 [get_ports {jesdTxN[0][3]}] ; #
+set_property PACKAGE_PIN AB2 [get_ports {jesdRxP[0][3]}] ; #
+set_property PACKAGE_PIN AB1 [get_ports {jesdRxN[0][3]}] ; #
+set_property PACKAGE_PIN AN4 [get_ports {jesdTxP[0][4]}] ; #
+set_property PACKAGE_PIN AN3 [get_ports {jesdTxN[0][4]}] ; #
+set_property PACKAGE_PIN AP2 [get_ports {jesdRxP[0][4]}] ; #
+set_property PACKAGE_PIN AP1 [get_ports {jesdRxN[0][4]}] ; #
+set_property PACKAGE_PIN AM6 [get_ports {jesdTxP[0][5]}] ; #
+set_property PACKAGE_PIN AM5 [get_ports {jesdTxN[0][5]}] ; #
+set_property PACKAGE_PIN AM2 [get_ports {jesdRxP[0][5]}] ; #
+set_property PACKAGE_PIN AM1 [get_ports {jesdRxN[0][5]}] ; #
+set_property PACKAGE_PIN AL4 [get_ports {jesdTxP[0][6]}] ; #
+set_property PACKAGE_PIN AL3 [get_ports {jesdTxN[0][6]}] ; #
+set_property PACKAGE_PIN AK2 [get_ports {jesdRxP[0][6]}] ; #
+set_property PACKAGE_PIN AK1 [get_ports {jesdRxN[0][6]}] ; #
+set_property PACKAGE_PIN T31 [get_ports {jesdTxP[0][7]}] ; #
+set_property PACKAGE_PIN T32 [get_ports {jesdTxN[0][7]}] ; #
+set_property PACKAGE_PIN R33 [get_ports {jesdRxP[0][7]}] ; #
+set_property PACKAGE_PIN R34 [get_ports {jesdRxN[0][7]}] ; #
+set_property PACKAGE_PIN P31 [get_ports {jesdTxP[0][8]}] ; #
+set_property PACKAGE_PIN P32 [get_ports {jesdTxN[0][8]}] ; #
+set_property PACKAGE_PIN N33 [get_ports {jesdRxP[0][8]}] ; #
+set_property PACKAGE_PIN N34 [get_ports {jesdRxN[0][8]}] ; #
+set_property PACKAGE_PIN M31 [get_ports {jesdTxP[0][9]}] ; #
+set_property PACKAGE_PIN M32 [get_ports {jesdTxN[0][9]}] ; #
+set_property PACKAGE_PIN L33 [get_ports {jesdRxP[0][9]}] ; #
+set_property PACKAGE_PIN L34 [get_ports {jesdRxN[0][9]}] ; #
+
+# JESD Reference Ports
+set_property -dict {IOSTANDARD LVDS DIFF_TERM TRUE } [get_ports {sysRefP[0][0]}] ; #jesdSysRefP 
+set_property -dict {IOSTANDARD LVDS DIFF_TERM TRUE } [get_ports {sysRefN[0][0]}] ; #jesdSysRefN
+
+# JESD ADC Sync Ports
+set_property -dict {IOSTANDARD LVDS} [get_ports {syncInP[0][3]}]  ; # jesdRxSyncP(0)
+set_property -dict {IOSTANDARD LVDS} [get_ports {syncInN[0][3]}]  ; # jesdRxSyncN(0)
+set_property -dict {IOSTANDARD LVDS} [get_ports {syncInP[0][0]}]  ; # jesdRxSyncP(1)
+set_property -dict {IOSTANDARD LVDS} [get_ports {syncInN[0][0]}]  ; # jesdRxSyncN(1)
+set_property -dict {IOSTANDARD LVDS DIFF_TERM TRUE } [get_ports {syncOutP[0][0]}] ; # jesdTxSyncP(0)
+set_property -dict {IOSTANDARD LVDS DIFF_TERM TRUE } [get_ports {syncOutN[0][0]}] ; # jesdTxSyncN(0)
+set_property -dict {IOSTANDARD LVDS DIFF_TERM TRUE } [get_ports {syncOutP[0][1]}] ; # jesdTxSyncP(1)
+set_property -dict {IOSTANDARD LVDS DIFF_TERM TRUE } [get_ports {syncOutN[0][1]}] ; # jesdTxSyncN(1)
+
+# ADC SPI
+set_property -dict {IOSTANDARD LVCMOS18 PULLUP true} [get_ports {spareP[0][2]}]    ; # adcSpiDo
+set_property -dict {IOSTANDARD LVCMOS18 PULLUP true} [get_ports {spareN[0][1]}]    ; # adcSpiClk
+set_property -dict {IOSTANDARD LVCMOS18 PULLUP true} [get_ports {spareN[0][2]}]    ; # adcSpiCsb(0)
+set_property -dict {IOSTANDARD LVCMOS18 PULLUP true} [get_ports {syncOutN[0][8]}]  ; # adcSpiCsb(1)
+set_property -dict {IOSTANDARD LVCMOS18 PULLUP true} [get_ports {syncOutP[0][9]}]  ; # adcSpiDi
+
+
+# DAC SPI
+set_property -dict {IOSTANDARD LVCMOS18 PULLUP true} [get_ports {spareP[0][0]}]    ; # dacSpiClk
+set_property -dict {IOSTANDARD LVCMOS18 PULLUP true} [get_ports {spareP[0][1]}]    ; # dacSpiDio
+set_property -dict {IOSTANDARD LVCMOS18 PULLUP true} [get_ports {spareN[0][0]}]    ; # dacSpiCsb(0)
+set_property -dict {IOSTANDARD LVCMOS18 PULLUP true} [get_ports {syncOutP[0][8]}]  ; # dacSpiCsb(1)
+
+# Hardware trigger
+set_property -dict {IOSTANDARD LVCMOS18} [get_ports {spareN[0][3]}]    ; # adcRst(0)
+set_property -dict {IOSTANDARD LVCMOS18} [get_ports {syncOutN[0][9]}]  ; # adcRst(1)
+
+
