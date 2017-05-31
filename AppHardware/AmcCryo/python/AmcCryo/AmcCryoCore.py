ffe6288eb83d10d8b34f59433cfc216c36434c7f
Jesus Vasquez <jvasquez@slac.stanford.edu>
2017-05-31 08:30:36 -0700

    Update PyRogue class for the Amc Cryo Board

--- AppHardware/AmcCryo/python/AmcCryo/AmcCryoCore.py
+++ AppHardware/AmcCryo/python/AmcCryo/AmcCryoCore.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 #-----------------------------------------------------------------------------
-# Title      : PyRogue Cryo Amc Rf Demo Board Core
+# Title      : PyRogue Cryo Amc Core
 #-----------------------------------------------------------------------------
-# File       : AmcCryoDemoCore.py
-# Created    : 2017-04-03
+# File       : AmcCryoCore.py
+# Created    : 2017-05-30
 #-----------------------------------------------------------------------------
 # Description:
-# PyRogue Cryo Amc Rf Demo Board Core
+# PyRogue Cryo Amc Core
 #-----------------------------------------------------------------------------
 # This file is part of the rogue software platform. It is subject to
 # the license terms in the LICENSE.txt file found in the top-level directory
@@ -22,8 +22,9 @@ import pyrogue as pr
 from surf._Adc16Dx370 import *
 from surf._Lmk04828 import *
 from surf._Dac38J84 import *
+from surf._Adc32Rf45 import *
 
-class AmcCryoDemoCore(pr.Device):
+class AmcCryoCore(pr.Device):
     def __init__(   self, 
                     name        = "AmcCryoDemoCore", 
                     description = "Cryo Amc Rf Demo Board Core", 
@@ -37,19 +38,20 @@ class AmcCryoDemoCore(pr.Device):
         # Variables
         ##############################
         
-        for i in range(3):
-          self.add(Adc16Dx370(
-                                  name         = "Adc16Dx370_%i" % (i),
+        for i in range(2):
+          self.add(Adc32Rf45(
+                                  name         = "Adc32Rf45_%i" % (i),
                                   offset       =  0x00020000 + (i * 0x00020000),
                               ))
         
         self.add(Lmk04828(
-                                offset       =  0x00080000,
+                                offset       = 0x000A0000,
                             ))
-        
-        self.add(Dac38J84(
-                                offset       =  0x000A0000,
-                            ))
+        for i in range(2):
+          self.add(Dac38J84(
+                                  name         = "Dac38J84_%i" % (i),
+                                  offset       =  0x00060000 + (i * 0x00020000),
+                              ))
 
         ##############################
         # Commands
@@ -58,14 +60,11 @@ class AmcCryoDemoCore(pr.Device):
         self.addCommand(    name         = "InitAmcCard",
                             description  = "Initialization for AMC card's JESD modules",
                             function     = """\
-                                           self.Adc16Dx370.CalibrateAdc.set(1)
                                            self.usleep(1000000)
                                            self.Lmk04828.PwrUpSysRef.set(1)
                                            self.usleep(1000000)
-                                           self.Dac38J84.itDac.set(1)
+                                           self.Dac38J84.InitDac.set(1)
                                            self.usleep(100000)
                                            self.Dac38J84.ClearAlarms.set(1)
-                                           self.usleep(100000)
-                                           self.ClearErrors.set(0)
                                            """
                         )
