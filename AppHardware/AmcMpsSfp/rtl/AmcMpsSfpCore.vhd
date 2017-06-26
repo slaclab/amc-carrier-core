e0ebc338d3f8095964cd48b4790341507c95cc53
Uros Legat <ulegat@slac.stanford.edu>
2017-06-26 15:57:23 +0200

    Fixing syntax err

--- AppHardware/AmcMpsSfp/rtl/AmcMpsSfpCore.vhd
+++ AppHardware/AmcMpsSfp/rtl/AmcMpsSfpCore.vhd
@@ -170,7 +170,7 @@ begin
             pllInc          => spareP(1),
             pllDec          => spareP(2),
             pllFrqTbl       => spareP(3),
-            pllDbl2By       => spareP(4),
+            pllBypass       => spareP(4),
             pllRate(0)      => spareP(5),
             pllRate(1)      => spareP(6),
             pllSFout(0)     => spareP(7),
