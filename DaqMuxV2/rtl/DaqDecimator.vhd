5b4b5b85f30c0d2f351904560e2de31d1778b60c
Larry Ruckman <ruckman@slac.stanford.edu>
2017-06-27 15:39:37 -0700

    adding pipelining to improve DaqDecimator performance

--- DaqMuxV2/rtl/DaqDecimator.vhd
+++ DaqMuxV2/rtl/DaqDecimator.vhd
@@ -2,7 +2,7 @@
 -- File       : DaqDecimator.vhd
 -- Company    : SLAC National Accelerator Laboratory
 -- Created    : 2015-04-15
--- Last update: 2016-05-24
+-- Last update: 2017-06-27
 -------------------------------------------------------------------------------
 -- Description: Reduces the sample rate:
 --                   averaging_i = '1':
@@ -32,87 +32,97 @@ use work.DaqMuxV2Pkg.all;
 
 entity DaqDecimator is
    generic (
-      TPD_G : time     := 1 ns
-      );
+      TPD_G : time := 1 ns);
    port (
       clk : in sl;
       rst : in sl;
 
       -- Sample data I/O
-      sampleData_i      : in  slv(31 downto 0);
-      decSampData_o     : out slv(31 downto 0);
-      
-      dec16or32_i       : in  sl;
-      averaging_i       : in  sl;
-      signed_i          : in  sl;
-      
-      rateDiv_i : in  slv(15 downto 0);
-      trig_i    : in  sl;
+      sampleData_i  : in  slv(31 downto 0);
+      decSampData_o : out slv(31 downto 0);
+
+      dec16or32_i : in sl;
+      averaging_i : in sl;
+      signed_i    : in sl;
+
+      rateDiv_i : in slv(15 downto 0);
+      trig_i    : in sl;
 
       -- Divided rate clk
-      rateClk_o : out sl
-      );
+      rateClk_o : out sl);
 end entity DaqDecimator;
 
 architecture rtl of DaqDecimator is
 
    type RegType is record
-      sampleData     : slv(sampleData_i'range);
-      cnt            : slv(15 downto 0);
-      divClk         : sl;
-      shft           : slv(1 downto 0);
-      prevFrame      : slv(15 downto 0);
-      sum            : slv(63 downto 0);
-      average        : slv(sampleData_i'range);
-      rateClk        : sl;
-      decSampleData  : slv(sampleData_i'range);
+      enable        : sl;
+      sampleData    : slv(sampleData_i'range);
+      cnt           : slv(15 downto 0);
+      cntPeriod     : slv(rateDiv_i'range);
+      divClk        : sl;
+      shft          : slv(1 downto 0);
+      prevFrame     : slv(15 downto 0);
+      sum           : slv(63 downto 0);
+      average       : slv(sampleData_i'range);
+      rateClk       : sl;
+      decSampleData : slv(sampleData_i'range);
    end record RegType;
 
    constant REG_INIT_C : RegType := (
-      sampleData     => (others => '0'),
-      cnt            => (others => '0'),
-      divClk         => '0',
-      shft           => "01",
-      prevFrame      => (others => '0'),      
-      sum            => (others => '0'),
-      average        => (others => '0'),
-      rateClk        => '0',
-      decSampleData  => (others => '0')
-   );
+      enable        => '0',
+      sampleData    => (others => '0'),
+      cnt           => (others => '0'),
+      cntPeriod     => (others => '0'),
+      divClk        => '0',
+      shft          => "01",
+      prevFrame     => (others => '0'),
+      sum           => (others => '0'),
+      average       => (others => '0'),
+      rateClk       => '0',
+      decSampleData => (others => '0'));
 
    signal r   : RegType := REG_INIT_C;
    signal rin : RegType;
-   
+
    signal s_countPeriod : slv(rateDiv_i'range);
-  
+
 begin
-   
+
    -- Divide count period by 2 if 16-bit
-   s_countPeriod <= rateDiv_i when dec16or32_i = '0' else '0'& rateDiv_i(rateDiv_i'left downto 1);
-   
-   
-   comb : process (r, rst, trig_i, rateDiv_i, s_countPeriod, sampleData_i, dec16or32_i, averaging_i, signed_i) is
-      variable v        : RegType;
+   s_countPeriod <= rateDiv_i when (dec16or32_i = '0') else ('0'& rateDiv_i(rateDiv_i'left downto 1));
+
+   comb : process (averaging_i, dec16or32_i, r, rateDiv_i, rst, s_countPeriod,
+                   sampleData_i, signed_i, trig_i) is
+      variable v : RegType;
    begin
+      -- Latch the current value
       v := r;
-      
-      -- Assign sample data according to different modes (or cases)
-      if (rateDiv_i <= 1) then    -- No data manipulation
-         v.sampleData := sampleData_i;
-      elsif (dec16or32_i = '0') then -- Signed mode 32 bit (if signed convert to unsigned)
-         v.sampleData := (sampleData_i(31) xor signed_i) & sampleData_i(30 downto 0);
-      elsif (dec16or32_i = '1') then -- Signed mode 16 bit (if signed convert to unsigned)
-         v.sampleData := (sampleData_i(31) xor signed_i) & sampleData_i(30 downto 16) &
-                         (sampleData_i(15) xor signed_i) & sampleData_i(14 downto  0);
+
+      -- Check if Decimator is disabled
+      if (rateDiv_i = 0) or (rateDiv_i = 1) then
+         v.enable := '0';
       else
+         v.enable := '1';
+      end if;
+
+      -- Assign sample data according to different modes (or cases)
+      if (r.enable = '0') then
+         -- No data manipulation
          v.sampleData := sampleData_i;
+      -- Signed mode 32 bit (if signed convert to unsigned)
+      elsif (dec16or32_i = '0') then
+         v.sampleData := (sampleData_i(31) xor signed_i) & sampleData_i(30 downto 0);
+      -- Signed mode 16 bit (if signed convert to unsigned)
+      else
+         v.sampleData(31 downto 16) := (sampleData_i(31) xor signed_i) & sampleData_i(30 downto 16);
+         v.sampleData(15 downto 0)  := (sampleData_i(15) xor signed_i) & sampleData_i(14 downto 0);
       end if;
 
       -- rateDiv clock generator 
-      if (s_countPeriod = (s_countPeriod'range => '0') or (rateDiv_i <= 1)) then
+      if (r.cntPeriod = 0) or (r.enable = '0') then
          v.cnt    := (others => '0');
          v.divClk := '1';
-      elsif (r.cnt >= s_countPeriod-1) then
+      elsif (r.cnt = (r.cntPeriod-1)) then
          v.cnt    := (others => '0');
          v.divClk := '1';
       else
@@ -120,8 +130,17 @@ begin
          v.divClk := '0';
       end if;
 
+      -- Update the count period local register
+      v.cntPeriod := s_countPeriod;
+
+      -- Check if s_countPeriod has changed
+      if (r.cntPeriod /= s_countPeriod) then
+         v.cnt    := (others => '0');
+         v.divClk := '1';
+      end if;
+
       -- Make a shifted control signal that indicates when to save and when to sample data
-      if (dec16or32_i = '0' or (rateDiv_i <= 1)) then
+      if (dec16or32_i = '0') or (r.enable = '0') then
          -- Shift and store disabled
          v.shft := "10";
       elsif (r.divClk = '1') then
@@ -130,54 +149,67 @@ begin
          v.shft := r.shft;
       end if;
 
-      -- Sum data
-      if (dec16or32_i = '0') then
-         v.sum := r.sum+r.sampleData;
-      else
-         v.sum := r.sum+r.sampleData(31 downto 16)+r.sampleData(15 downto 0);
-      end if;
-      
-      -- Power of 2 Divide
-      v.average := power2div(v.sum,rateDiv_i);
-      
       -- Zero data if period reached
       -- so next sum starts fresh
       if (r.divClk = '1') then
-         v.sum := (others => '0');
+         if (dec16or32_i = '0') then
+            -- 32-bit summation
+            v.sum(31 downto 0)  := r.sampleData;
+            v.sum(63 downto 32) := (others => '0');
+         else
+            -- 32-bit summation
+            v.sum(31 downto 0)  := (x"0000" & r.sampleData(31 downto 16))+(x"0000" & r.sampleData(15 downto 0));
+            v.sum(63 downto 32) := (others => '0');
+         end if;
+      else
+         if (dec16or32_i = '0') then
+            -- 32-bit summation
+            v.sum := r.sum+(x"00000000" & r.sampleData);
+         else
+            -- 32-bit summation
+            v.sum := r.sum+(x"000000000000" & r.sampleData(31 downto 16))+(x"000000000000" & r.sampleData(15 downto 0));
+         end if;
       end if;
-      
+
       -- Bypass average if disabled 
-      if (averaging_i ='0') then
+      if (averaging_i = '0') then
          v.average := r.sampleData;
-      end if;      
-      
+      elsif (r.divClk = '1') then
+         -- Power of 2 Divide
+         v.average := power2div(r.sum, rateDiv_i);
+      end if;
+
       -- Save frame
-      if (r.divClk = '1' and r.shft = "01") then
-         v.prevFrame := v.average(15 downto 0);
+      if (r.divClk = '1') and (r.shft = "01") then
+         v.prevFrame := r.average(15 downto 0);
       else
          v.prevFrame := r.prevFrame;
       end if;
-      
+
       -- Register decimated Sample data
-      if (rateDiv_i <= 1) then
+      if (r.enable = '0') then
          v.decSampleData := r.sampleData;
-      elsif (dec16or32_i = '1') then -- 16 bit: if signed convert back to signed
-         v.decSampleData := (v.average(15)   xor signed_i) & v.average(14 downto 0) &  
-                            (r.prevFrame(15) xor signed_i) & r.prevFrame(14 downto 0);
-      else -- 32 bit: if signed convert back to signed
-         v.decSampleData := (v.average(31)   xor signed_i) & v.average(30 downto 0);
+      else
+         -- 32 bit: if signed convert back to signed
+         if (dec16or32_i = '0') then
+            v.decSampleData := (r.average(31) xor signed_i) & r.average(30 downto 0);
+         -- 16 bit: if signed convert back to signed
+         else
+            v.decSampleData(31 downto 16) := (r.average(15) xor signed_i) & r.average(14 downto 0);
+            v.decSampleData(15 downto 0)  := (r.prevFrame(15) xor signed_i) & r.prevFrame(14 downto 0);
+         end if;
       end if;
-            
+
       -- Register rate clock (decimated data strobe)
-      if (r.divClk = '1' and r.shft = "10") then
+      if (r.divClk = '1') and (r.shft = "10") then
          v.rateClk := '1';
       else
          v.rateClk := '0';
       end if;
-      
+
       -- If disabled zero some of the data
       -- trig_i also resets the module and therefore syncs internal counters of all lanes
-      if (trig_i = '1' and rateDiv_i > 1) then
+      if (trig_i = '1') and (r.enable = '1') then
          v.divClk    := '0';
          v.rateClk   := '0';
          v.cnt       := (others => '0');
@@ -185,13 +217,18 @@ begin
          v.shft      := "01";
          v.sum       := (others => '0');
       end if;
-      
-      -- Reset
+
+      -- Synchronous Reset
       if (rst = '1') then
          v := REG_INIT_C;
       end if;
 
+      -- Register the variable for next clock cycle
       rin <= v;
+
+      -- Outputs
+      rateClk_o     <= r.rateClk;
+      decSampData_o <= r.decSampleData;
 
    end process comb;
 
@@ -202,8 +239,4 @@ begin
       end if;
    end process seq;
 
-   -- Output assignment
-   rateClk_o      <= r.rateClk;
-   decSampData_o  <= r.decSampleData;
----------------------------------------   
-end architecture rtl;
+end rtl;
--- DaqMuxV2/tb/DaqDecimatorTb.vhd
+++ DaqMuxV2/tb/DaqDecimatorTb.vhd
@@ -1,3 +1,10 @@
+-------------------------------------------------------------------------------
+-- File       : DaqDecimatorTb.vhd
+-- Company    : SLAC National Accelerator Laboratory
+-- Created    : 2015-04-15
+-- Last update: 2017-06-27
+-------------------------------------------------------------------------------
+-- Description: Simulation Testbed for DaqDecimator
 ------------------------------------------------------------------------------
 -- This file is part of 'LCLS2 Common Carrier Core'.
 -- It is subject to the license terms in the LICENSE.txt file found in the 
@@ -7,6 +14,7 @@
 -- may be copied, modified, propagated, or distributed except according to 
 -- the terms contained in the LICENSE.txt file.
 ------------------------------------------------------------------------------
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
@@ -14,94 +22,86 @@ use ieee.std_logic_arith.all;
 
 use work.StdRtlPkg.all;
 
---------------------------------------------------------------------------------
-entity  DaqDecimatorTb is
+entity DaqDecimatorTb is
+end entity;
 
-end entity ;
---------------------------------------------------------------------------------
+architecture tb of DaqDecimatorTb is
 
+   constant CLK_PERIOD_C : time     := 4 ns;
+   constant TPD_C        : time     := 1 ns;
+   constant INCR_C       : positive := 4;
 
-architecture Bhv of DaqDecimatorTb is
-  -----------------------------
-  -- Port Signals 
-  -----------------------------
-   constant CLK_PERIOD_C : time    := 10 ns;
-   constant TPD_C        : time    := 1 ns; 
+   constant dec16or32_c : sl               := '1';  -- '0' = 32b format, '1' = 16b format
+   constant averaging_c : sl               := '1';  -- '0' = no averaging, '1' = averaging
+   constant rateDiv_c   : slv(15 downto 0) := x"0004";  -- rate divide by 
 
-   -- Clocking
-   signal   clk_i                 : sl := '0';
-   signal   rst_i                 : sl := '0';
-   
-   signal   sampleData_i    : slv(31 downto 0) := x"0000_0000";
-   signal   dec16or32_i     : sl :='0';
-   signal   rateDiv_i       : slv(15 downto 0) := x"0002";
-   signal   trig_i          : sl :='0';
-   signal   averaging_i     : sl :='1';  
-   ----------------------------------------------
+   signal clk_i         : sl               := '0';
+   signal rst_i         : sl               := '0';
+   signal trig_i        : sl               := '0';
+   signal rateClk_o     : sl               := '0';
+   signal sampleData_i  : slv(31 downto 0) := x"0000_0000";
+   signal decSampData_o : slv(31 downto 0) := (others => '0');
+   signal s_cnt         : slv(31 downto 0) := (others => '0');
 
-   signal   rateClk_o       : sl;
-   signal   decSampData_o   : slv(31 downto 0);
-   
-   signal   s_cnt   : slv(31 downto 0);
-begin  
+begin
 
+   -----------------------------
    -- Generate clocks and resets
-   DDR_ClkRst_Inst : entity work.ClkRst
-   generic map (
-     CLK_PERIOD_G      => CLK_PERIOD_C,
-     RST_START_DELAY_G => 1 ns,     -- Wait this long into simulation before asserting reset
-     RST_HOLD_TIME_G   => 1000 ns)  -- Hold reset for this long)
-   port map (
-     clkP => clk_i,
-     clkN => open,
-     rst  => rst_i,
-     rstL => open);
+   -----------------------------
+   U_ClkRst : entity work.ClkRst
+      generic map (
+         CLK_PERIOD_G      => CLK_PERIOD_C,
+         RST_START_DELAY_G => 1 ns,  -- Wait this long into simulation before asserting reset
+         RST_HOLD_TIME_G   => 1000 ns)  -- Hold reset for this long)
+      port map (
+         clkP => clk_i,
+         clkN => open,
+         rst  => rst_i,
+         rstL => open);
 
-  -----------------------------
-  -- component instantiation 
-  -----------------------------
-  AmcA_INST: entity work.DaqDecimator
-   generic map (
-      TPD_G => TPD_C)
-   port map (
-      clk             => clk_i,
-      rst             => rst_i,
-      sampleData_i    => sampleData_i,
-      decSampData_o   => decSampData_o,
-      dec16or32_i     => dec16or32_i,
-      rateDiv_i       => rateDiv_i,
-      trig_i          => trig_i,
-      averaging_i     => averaging_i,
-      rateClk_o       => rateClk_o);
+   ----------------------
+   -- DaqDecimator Module
+   ----------------------
+   U_DaqDecimator : entity work.DaqDecimator
+      generic map (
+         TPD_G => TPD_C)
+      port map (
+         clk           => clk_i,
+         rst           => rst_i,
+         sampleData_i  => sampleData_i,
+         decSampData_o => decSampData_o,
+         dec16or32_i   => dec16or32_c,
+         averaging_i   => averaging_c,
+         signed_i      => '0',          -- '0' = unsigned, '1' = signed
+         rateDiv_i     => rateDiv_c,
+         trig_i        => trig_i,
+         rateClk_o     => rateClk_o);
 
-	
    seq : process (clk_i) is
    begin
       if (rising_edge(clk_i)) then
-         if (rst_i = '1') then  
-            s_cnt <= (others=>'0');
-         elsif dec16or32_i = '0' then
-            s_cnt <= s_cnt + 1 after TPD_C;
+         if (rst_i = '1') then
+            s_cnt <= (others => '0');
+         elsif dec16or32_c = '0' then
+            s_cnt <= s_cnt + INCR_C after TPD_C;
          else
-            s_cnt <= s_cnt + 2 after TPD_C;
+            s_cnt <= s_cnt + 2*INCR_C after TPD_C;
          end if;
       end if;
    end process seq;
-   
-   
-   sampleData_i <= s_cnt(15 downto 0)+1 & s_cnt(15 downto 0) when dec16or32_i = '1' else s_cnt;
-   
-   
+
+   sampleData_i <= s_cnt(15 downto 0)+INCR_C & s_cnt(15 downto 0) when (dec16or32_c = '1') else s_cnt;
+
    StimuliProcess : process
    begin
       wait until rst_i = '0';
 
-      wait for CLK_PERIOD_C*200;
-      trig_i <= '1';    
+      wait for CLK_PERIOD_C*800;
+      trig_i <= '1';
       wait for CLK_PERIOD_C*1;
       trig_i <= '0';
-      
+
       wait;
    end process StimuliProcess;
-  
-end architecture Bhv;
+
+end tb;
