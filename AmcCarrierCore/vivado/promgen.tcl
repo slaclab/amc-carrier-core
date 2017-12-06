3017e06b5312fb90571f266211fe87dcfdc44304
Larry Ruckman <ruckman@slac.stanford.edu>
2017-12-06 14:50:51 -0800

    Adding future KU095/KU11P/KU15P support

--- AmcCarrierCore/vivado/promgen.tcl
+++ AmcCarrierCore/vivado/promgen.tcl
@@ -13,30 +13,9 @@ set format     "mcs"
 set inteface   "SPIx1"
 set size       "1024"
 
-# XCKU040/XCKU060 .BIT file locations
+# .BIT file locations
 set FSBL_BIT    "0x00000000"
-set LCLS_I_BIT  "0x02000000"
 set LCLS_II_BIT "0x04000000"
-set TEMP_BIT    "0x06000000"
-
-# XCKU040 .TAR.GZ file locations (up to 17 MB file)
-set FSBL_GZ    "0x00F43EFC"
-set LCLS_I_GZ  "0x02F43EFC"
-set LCLS_II_GZ "0x04F43EFC"
-set TEMP_GZ    "0x06F43EFC"
-
-# XCKU060 .TAR.GZ file locations (up to 9 MB file)
-set FSBL_GZ_KU060    "0x01701DEC"
-set LCLS_I_GZ_KU060  "0x03701DEC"
-set LCLS_II_GZ_KU060 "0x05701DEC"
-set TEMP_GZ_KU060    "0x07701DEC"
 
 # File Paths
 set BIT_PATH   "$::env(IMPL_DIR)/$::env(PROJECT).bit"
-
-# Check for ruck v1.2.0 (or later)
-if { [file exists $::env(IMAGES_DIR)/$::env(IMAGENAME).cpsw.tar.gz] == 1 } {
-   set DATA_PATH  "$::env(IMAGES_DIR)/$::env(IMAGENAME).cpsw.tar.gz"
-} else {
-   set DATA_PATH  "$::env(IMAGES_DIR)/$::env(IMAGENAME).tar.gz"
-}
