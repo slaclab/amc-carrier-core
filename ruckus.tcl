9d09fd949c1248e8a408908307803d4ec333f23e
Larry Ruckman <ruckman@slac.stanford.edu>
2017-12-15 22:21:17 -0800

    preparing for the next merge

--- ruckus.tcl
+++ ruckus.tcl
@@ -1,13 +1,32 @@
 # Load RUCKUS environment and library
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
-## Check for submodule tagging
-if { [SubmoduleCheck {ruckus}             {1.5.3} ] < 0 } {exit -1}
-if { [SubmoduleCheck {surf}               {1.4.5} ] < 0 } {exit -1}
-if { [SubmoduleCheck {lcls-timing-core}   {1.7.3} ] < 0 } {exit -1}
+# Get the family type
+set family [getFpgaFamily]
 
-## Check for version 2016.4 of Vivado
-if { [VersionCheck 2016.4 "mustBeExact"] < 0 } {exit -1}
+# Check for submodule tagging
+if { [SubmoduleCheck {ruckus}             {1.5.5} ] < 0 } {exit -1}
+if { [SubmoduleCheck {surf}               {1.5.0} ] < 0 } {exit -1}
+if { [SubmoduleCheck {lcls-timing-core}   {1.8.0} ] < 0 } {exit -1}
+
+# Check for Kintex Ultrascale+
+if { ${family} == "kintexuplus" } {
+   ## Check for version 2017.3 of Vivado
+   if { [VersionCheck 2017.3 "mustBeExact"] < 0 } {
+      exit -1
+   }
+# Check Kintex Ultrascale
+} elseif { ${family} == "kintexu" } {  
+   # Check for version 2016.4 of Vivado
+   if { [VersionCheck 2016.4] < 0 } {
+      ## Check for version 2017.3 of Vivado
+      if { [VersionCheck 2017.3 "mustBeExact"] < 0 } {
+         exit -1
+      }
+   }
+} else { 
+   puts "\n\nERROR: Invalid PRJ_PART was defined in the Makefile\n\n"; exit -1
+}
 
 # Check if required variables exist
 if { [info exists ::env(AMC_ADV_BUILD)] != 1 }  {puts "\n\nERROR: AMC_ADV_BUILD is not defined in $::env(PROJ_DIR)/Makefile\n\n";   exit -1}
