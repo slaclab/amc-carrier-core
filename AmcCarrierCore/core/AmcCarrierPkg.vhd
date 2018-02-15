e955176f2fa6c57ef67b9856e0b9104892e6468b
Larry Ruckman <ruckman@slac.stanford.edu>
2018-02-15 08:23:23 -0800

    syncing up with LCLS-II v2.1.6 tag

--- AmcCarrierCore/core/AmcCarrierPkg.vhd
+++ AmcCarrierCore/core/AmcCarrierPkg.vhd
@@ -2,7 +2,7 @@
 -- File       : AmcCarrierPkg.vhd
 -- Company    : SLAC National Accelerator Laboratory
 -- Created    : 2015-09-08
--- Last update: 2018-01-11
+-- Last update: 2018-02-12
 -------------------------------------------------------------------------------
 -- Description: Common AMC Carrier Core VHDL package
 -------------------------------------------------------------------------------
@@ -52,7 +52,9 @@ package AmcCarrierPkg is
    -- 12/23/2017 (0x02010200): https://github.com/slaclab/amc-carrier-core/releases/tag/v2.1.2
    -- 01/08/2018 (0x02010300): https://github.com/slaclab/amc-carrier-core/releases/tag/v2.1.3
    -- 01/11/2018 (0x02010400): https://github.com/slaclab/amc-carrier-core/releases/tag/v2.1.4
-   constant AMC_CARRIER_CORE_VERSION_C : slv(31 downto 0) := x"02_01_04_00";
+   -- 01/11/2018 (0x02010500): https://github.com/slaclab/amc-carrier-core/releases/tag/v2.1.5
+   -- 02/12/2018 (0x02010600): https://github.com/slaclab/amc-carrier-core/releases/tag/v2.1.6
+   constant AMC_CARRIER_CORE_VERSION_C : slv(31 downto 0) := x"02_01_06_00";
 
    -----------------------------------------------------------
    -- Application: Configurations, Constants and Records Types
