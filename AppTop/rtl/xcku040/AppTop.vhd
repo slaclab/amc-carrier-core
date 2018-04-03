51db23005810c77ad9cb02e725f4aa18da15270b
Till Straumann <strauman@slac.stanford.edu>
2018-04-03 08:10:44 -0700

    FIXED: AppTop's missing connection of obAppDebugMaster/Slave

--- AppTop/rtl/xcku040/AppTop.vhd
+++ AppTop/rtl/xcku040/AppTop.vhd
@@ -203,9 +203,6 @@ architecture mapping of AppTop is
    signal dacSigValids : Slv7Array(1 downto 0);
    signal dacSigValues : sampleDataVectorArray(1 downto 0, 6 downto 0);
 
-   signal obAppDbgMaster : AxiStreamMasterType;
-   signal obAppDbgSlave  : AxiStreamSlaveType;
-
 begin
 
    --------------------------
@@ -507,8 +504,8 @@ begin
          ibBpMsgServerMaster => ibBpMsgServerMaster,
          ibBpMsgServerSlave  => ibBpMsgServerSlave,
          -- Application Debug Interface (axilClk domain)
-         obAppDebugMaster    => obAppDbgMaster,
-         obAppDebugSlave     => obAppDbgSlave,
+         obAppDebugMaster    => obAppDebugMaster,
+         obAppDebugSlave     => obAppDebugSlave,
          ibAppDebugMaster    => ibAppDebugMaster,
          ibAppDebugSlave     => ibAppDebugSlave,
          -- MPS Concentrator Interface (axilClk domain)
