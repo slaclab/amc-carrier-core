80281bb0ccf76e128fea4b287f3c6be2c6570f16
Larry Ruckman <ruckman@slac.stanford.edu>
2019-01-16 09:18:14 -0800

    Update ruckus.tcl
    Updating submodule locks

--- ruckus.tcl
+++ ruckus.tcl
@@ -37,7 +37,7 @@ set family [getFpgaFamily]
 if { [info exists ::env(OVERRIDE_SUBMODULE_LOCKS)] != 1 || $::env(OVERRIDE_SUBMODULE_LOCKS) == 0 } {
    if { [SubmoduleCheck {lcls-timing-core} {1.12.4} "mustBeExact" ] < 0 } {exit -1}
    if { [SubmoduleCheck {ruckus}           {1.7.1}  "mustBeExact" ] < 0 } {exit -1}
-   if { [SubmoduleCheck {surf}             {1.9.3}  "mustBeExact" ] < 0 } {exit -1}
+   if { [SubmoduleCheck {surf}             {1.9.4}  "mustBeExact" ] < 0 } {exit -1}
 } else {
    puts "\n\n*********************************************************"
    puts "OVERRIDE_SUBMODULE_LOCKS != 0"
