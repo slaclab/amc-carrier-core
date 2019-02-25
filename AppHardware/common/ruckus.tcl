d713950d5a2f2f09035b5c5d2ca1ae2720f7f79a
Larry Ruckman <ruckman@slac.stanford.edu>
2019-02-25 11:40:13 -0800

    ruckus.tcl bug fix

--- AppHardware/common/ruckus.tcl
+++ AppHardware/common/ruckus.tcl
@@ -8,8 +8,12 @@ set family [getFpgaFamily]
 loadSource -dir "$::DIR_PATH/rtl"
 
 # Load AMC BAY[0] constraints files
-if { $::env(AMC_TYPE_BAY0) eq {AmcMpsSfp} } {
-   set temp "AmcMpsSfp uses special JESD mapping"
+if { $::env(AMC_TYPE_BAY0) eq {AmcMrLlrfUpConvert}  ||
+     $::env(AMC_TYPE_BAY0) eq {AmcMrLlrfDownConvert}||
+     $::env(AMC_TYPE_BAY0) eq {AmcCryoDemo}||
+     $::env(AMC_TYPE_BAY0) eq {AmcStriplineBpm}||
+     $::env(AMC_TYPE_BAY0) eq {AmcMpsSfp} } {
+   set temp "$::env(AMC_TYPE_BAY0) uses special JESD mapping"
 } else {
    loadConstraints -path "$::DIR_PATH/xdc/${family}/StandardJesdMapBay0.xdc"  
    if { $::env(PRJ_PART) eq {XCKU060-FFVA1156-2-E} ||
@@ -19,8 +23,12 @@ if { $::env(AMC_TYPE_BAY0) eq {AmcMpsSfp} } {
 }
 
 # Load AMC BAY[1] constraints files
-if { $::env(AMC_TYPE_BAY1) eq {AmcMpsSfp} } {
-   set temp "AmcMpsSfp uses special JESD mapping"
+if { $::env(AMC_TYPE_BAY1) eq {AmcMrLlrfUpConvert}  ||
+     $::env(AMC_TYPE_BAY1) eq {AmcMrLlrfDownConvert}||
+     $::env(AMC_TYPE_BAY1) eq {AmcCryoDemo}||
+     $::env(AMC_TYPE_BAY1) eq {AmcStriplineBpm}||
+     $::env(AMC_TYPE_BAY1) eq {AmcMpsSfp} } {
+   set temp "$::env(AMC_TYPE_BAY1) uses special JESD mapping"
 } else {
    loadConstraints -path "$::DIR_PATH/xdc/${family}/StandardJesdMapBay1.xdc"  
    if { $::env(PRJ_PART) eq {XCKU060-FFVA1156-2-E} ||
