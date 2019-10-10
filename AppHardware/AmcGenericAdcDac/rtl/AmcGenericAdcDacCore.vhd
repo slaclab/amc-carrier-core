a694b48b1d34d5324b15ed3e44919797d2c2234c
Larry Ruckman <ruckman@slac.stanford.edu>
2019-10-10 10:52:47 -0700

    settting AmcGenericAdcDacCore.U_jesdSysRef.GEN_ASYNC_G=FALSE

--- AppHardware/AmcGenericAdcDac/rtl/AmcGenericAdcDacCore.vhd
+++ AppHardware/AmcGenericAdcDac/rtl/AmcGenericAdcDacCore.vhd
@@ -329,8 +329,9 @@ begin
 
    U_jesdSysRef : entity work.JesdSyncIn
       generic map (
-         TPD_G    => TPD_G,
-         INVERT_G => false)
+         TPD_G       => TPD_G,
+         GEN_ASYNC_G => false, -- Deskewed using LMK to get rid of race condition between jesdSysRefP/N and jesdClk
+         INVERT_G    => true)  -- Note inverted because it is Swapped on the board
       port map (
          -- Clock
          jesdClk   => jesdClk,
