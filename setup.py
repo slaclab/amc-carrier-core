7fc132005fa7c212dc772b4f22845d2a507426e8
Ryan Herbst <rherbst@slac.stanford.edu>
2020-02-12 10:28:49 -0800

    Add conda recipe

---
+++ setup.py
@@ -0,0 +1,33 @@
+
+from distutils.core import setup
+from git import Repo
+
+repo = Repo()
+
+# Get version before adding version file
+ver = repo.git.describe('--tags')
+
+# append version constant to package init
+with open('python/AmcCarrierCore/__init__.py','a') as vf:
+    vf.write(f'\n__version__="{ver}"\n')
+
+setup (
+   name='amc_carrier_core',
+   version=ver,
+   packages=[ 'AmcCarrierCore',
+              'AmcCarrierCore/AppHardware',
+              'AmcCarrierCore/AppHardware/AmcCryo',
+              'AmcCarrierCore/AppHardware/AmcCryoDemo',
+              'AmcCarrierCore/AppHardware/AmcGenericAdcDac',
+              'AmcCarrierCore/AppHardware/AmcMicrowaveMux',
+              'AmcCarrierCore/AppHardware/RtmCryoDet',
+              'AmcCarrierCore/AppHardware/RtmDigitalDebug',
+              'AmcCarrierCore/AppMps',
+              'AmcCarrierCore/AppTop',
+              'AmcCarrierCore/AxisBramRingBuffer',
+              'AmcCarrierCore/BsaCore',
+              'AmcCarrierCore/DacSigGen',
+              'AmcCarrierCore/DaqMuxV2',],
+   package_dir={'':'python'},
+)
+
