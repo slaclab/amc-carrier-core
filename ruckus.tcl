432f394b84458535872d3afbe5ef0218e497972a
Larry Ruckman <ruckman@slac.stanford.edu>
2020-05-18 09:25:47 -0700

    Updating submodule locks

--- ruckus.tcl
+++ ruckus.tcl
@@ -36,8 +36,8 @@ set family [getFpgaFamily]
 # Check for submodule tagging
 if { [info exists ::env(OVERRIDE_SUBMODULE_LOCKS)] != 1 || $::env(OVERRIDE_SUBMODULE_LOCKS) == 0 } {
    if { [SubmoduleCheck {lcls-timing-core} {2.0.0} "mustBeExact" ] < 0 } {exit -1}
-   if { [SubmoduleCheck {ruckus}           {2.4.0} "mustBeExact" ] < 0 } {exit -1}
-   if { [SubmoduleCheck {surf}             {2.3.0} "mustBeExact" ] < 0 } {exit -1}
+   if { [SubmoduleCheck {ruckus}           {2.5.0} "mustBeExact" ] < 0 } {exit -1}
+   if { [SubmoduleCheck {surf}             {2.5.0} "mustBeExact" ] < 0 } {exit -1}
 } else {
    puts "\n\n*********************************************************"
    puts "OVERRIDE_SUBMODULE_LOCKS != 0"
