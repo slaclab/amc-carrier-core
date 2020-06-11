dcc0422f2bd2d68d3388f9c98470f1a34b1f8bbd
Larry Ruckman <ruckman@slac.stanford.edu>
2020-06-11 11:44:01 -0700

    Preparing for release

--- AmcCarrierCore/core/AmcCarrierPkg.vhd
+++ AmcCarrierCore/core/AmcCarrierPkg.vhd
@@ -25,8 +25,8 @@ use lcls_timing_core.TimingPkg.all;
 
 package AmcCarrierPkg is
 
-   -- https://github.com/slaclab/amc-carrier-core/releases/tag/v3.3.0
-   constant AMC_CARRIER_CORE_VERSION_C : slv(31 downto 0) := x"03_03_00_00";
+   -- https://github.com/slaclab/amc-carrier-core/releases/tag/v3.4.0
+   constant AMC_CARRIER_CORE_VERSION_C : slv(31 downto 0) := x"03_04_00_00";
 
    -----------------------------------------------------------
    -- Application: Configurations, Constants and Records Types
