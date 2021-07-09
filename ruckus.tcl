f90afd9c3ff760b80bc625f1030e4d2149bb7cab
Larry Ruckman <ruckman@slac.stanford.edu>
2021-07-09 10:01:47 -0700

    adding Vivado 2021.1 support

--- ruckus.tcl
+++ ruckus.tcl
@@ -5,7 +5,7 @@ proc MyVersionCheck { } {
 
    # Get the Vivado version
    set VersionNumber [version -short]
-   set supported "2018.3 2019.1 2019.2 2020.1 2020.2"
+   set supported "2018.3 2019.1 2019.2 2020.1 2020.2 2021.1"
    set retVar -1
 
    # Generate error message
