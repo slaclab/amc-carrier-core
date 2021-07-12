f4d2c52ecbad47bdb3a6f444c58201a230dd4f1a
Larry Ruckman <ruckman@slac.stanford.edu>
2021-07-12 10:33:36 -0700

    release prep

--- AmcCarrierCore/core/AmcCarrierPkg.vhd
+++ AmcCarrierCore/core/AmcCarrierPkg.vhd
@@ -25,8 +25,8 @@ use lcls_timing_core.TimingPkg.all;
 
 package AmcCarrierPkg is
 
-   -- https://github.com/slaclab/amc-carrier-core/releases/tag/v4.3.1
-   constant AMC_CARRIER_CORE_VERSION_C : slv(31 downto 0) := x"04_03_01_00";
+   -- https://github.com/slaclab/amc-carrier-core/releases/tag/v4.4.0
+   constant AMC_CARRIER_CORE_VERSION_C : slv(31 downto 0) := x"04_04_00_00";
 
    -----------------------------------------------------------
    -- Application: Configurations, Constants and Records Types
--- ruckus.tcl
+++ ruckus.tcl
@@ -37,7 +37,7 @@ set family [getFpgaFamily]
 if { [info exists ::env(OVERRIDE_SUBMODULE_LOCKS)] != 1 || $::env(OVERRIDE_SUBMODULE_LOCKS) == 0 } {
    if { [SubmoduleCheck {lcls-timing-core} {3.4.0}  "mustBeExact" ] < 0 } {exit -1}
    if { [SubmoduleCheck {ruckus}           {3.1.0}  "mustBeExact" ] < 0 } {exit -1}
-   if { [SubmoduleCheck {surf}             {2.22.0} "mustBeExact" ] < 0 } {exit -1}
+   if { [SubmoduleCheck {surf}             {2.23.0} "mustBeExact" ] < 0 } {exit -1}
 } else {
    puts "\n\n*********************************************************"
    puts "OVERRIDE_SUBMODULE_LOCKS != 0"
