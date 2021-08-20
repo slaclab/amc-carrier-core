3024f4078092f67a7c47d8a2fa77f45472a3eb12
Larry Ruckman <ruckman@slac.stanford.edu>
2021-08-20 11:26:47 -0700

    updating surf submodule lock to v2.24.1

--- ruckus.tcl
+++ ruckus.tcl
@@ -1,63 +1,21 @@
 # Load RUCKUS environment and library
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
-proc MyVersionCheck { } {
-
-   # Get the Vivado version
-   set VersionNumber [version -short]
-   set supported "2018.3 2019.1 2019.2 2020.1 2020.2 2021.1"
-   set retVar -1
-
-   # Generate error message
-   set errMsg "\n\n*********************************************************\n"
-   set errMsg "${errMsg}Your Vivado Version Vivado   = ${VersionNumber}\n"
-   set errMsg "${errMsg}However, Vivado Version Lock = ${supported}\n"
-   set errMsg "${errMsg}You need to change your Vivado software to one of these versions\n"
-   set errMsg "${errMsg}*********************************************************\n\n"
-
-   # Loop through the different support version list
-   foreach pntr ${supported} {
-      if { ${VersionNumber} == ${pntr} } {
-         set retVar 0
-      }
-   }
-
-   # Check for no support version detected
-   if  { ${retVar} < 0 } {
-      puts ${errMsg}
-   }
-
-   return ${retVar}
+## Check for Vivado version 2018.3 (or later)
+if { [VersionCheck 2018.3 ] < 0 } {
+   exit -1
 }
-
-# Get the family type
-set family [getFpgaFamily]
 
 # Check for submodule tagging
 if { [info exists ::env(OVERRIDE_SUBMODULE_LOCKS)] != 1 || $::env(OVERRIDE_SUBMODULE_LOCKS) == 0 } {
    if { [SubmoduleCheck {lcls-timing-core} {3.5.0}  "mustBeExact" ] < 0 } {exit -1}
    if { [SubmoduleCheck {ruckus}           {3.1.1}  "mustBeExact" ] < 0 } {exit -1}
-   if { [SubmoduleCheck {surf}             {2.23.6} "mustBeExact" ] < 0 } {exit -1}
+   if { [SubmoduleCheck {surf}             {2.24.1} "mustBeExact" ] < 0 } {exit -1}
 } else {
    puts "\n\n*********************************************************"
    puts "OVERRIDE_SUBMODULE_LOCKS != 0"
    puts "Ignoring the submodule locks in amc-carrier-core/ruckus.tcl"
    puts "*********************************************************\n\n"
-}
-
-# Check for Kintex Ultrascale+
-if { ${family} == "kintexuplus" } {
-   ## Check for Vivado version 2018.2 (or later)
-   if { [VersionCheck 2018.2 ] < 0 } {
-      exit -1
-   }
-# Check Kintex Ultrascale
-} elseif { ${family} == "kintexu" } {
-   if { [MyVersionCheck] < 0 } {
-      exit -1
-   }
-} else {
-   puts "\n\nERROR: Invalid PRJ_PART was defined in the Makefile\n\n"; exit -1
 }
 
 # Check if required variables exist
