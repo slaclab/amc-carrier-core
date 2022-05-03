e14320e58d70239895c141e0c0dc97cbb603b418
Larry Ruckman <ruckman@slac.stanford.edu>
2022-05-03 13:01:15 -0700

    updating submodule lock

--- ruckus.tcl
+++ ruckus.tcl
@@ -8,7 +8,7 @@ if { [VersionCheck 2018.3 ] < 0 } {
 
 # Check for submodule tagging
 if { [info exists ::env(OVERRIDE_SUBMODULE_LOCKS)] != 1 || $::env(OVERRIDE_SUBMODULE_LOCKS) == 0 } {
-    if { [SubmoduleCheck {lcls-timing-core} {3.6.3} "mustBeExact" ] < 0 } {exit -1}
+   if { [SubmoduleCheck {lcls-timing-core} {3.6.4}  "mustBeExact" ] < 0 } {exit -1}
    if { [SubmoduleCheck {ruckus}           {4.1.2}  "mustBeExact" ] < 0 } {exit -1}
    if { [SubmoduleCheck {surf}             {2.26.0} "mustBeExact" ] < 0 } {exit -1}
 } else {
