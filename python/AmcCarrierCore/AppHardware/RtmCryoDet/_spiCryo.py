1373b021a22a31181ba6be408bcf7fc82a19803e
Shawn W. Henderson <20823858+swh76@users.noreply.github.com>
2023-06-12 12:17:20 -0700

    Commenting out unfinished attempt to move cryocard readback and control into rogue.

--- python/AmcCarrierCore/AppHardware/RtmCryoDet/_spiCryo.py
+++ python/AmcCarrierCore/AppHardware/RtmCryoDet/_spiCryo.py
@@ -26,24 +26,24 @@ class SpiCryo(pr.Device):
             **kwargs):
         super().__init__(name=name, description=description, **kwargs)
 
-        ## _rawWrite/_rawRead
-        self.write_address = 0x0
-        self.read_address  = 0x4
-
-        self.relay_address = 0x2
-        self.hemt_bias_address = 0x3
-        self.a50K_bias_address = 0x4
-        self.temperature_address = 0x5
-        self.cycle_count_address = 0x6  # used for testing
-        self.ps_en_address = 0x7 # PS enable (HEMT: bit 0, 50k: bit 1)
-        self.ac_dc_status_address = 0x8 # AC/DC mode status (bit 0: FRN_RLY, bit 1: FRP_RLY)
-        self.adc_scale = 3.3/(1024.0 * 5)
-        self.temperature_scale = 1/.028 # was 100
-        self.temperature_offset =.25
-        self.bias_scale = 1.0
-        self.max_retries = 5  #number of re-tries waiting for response
-        self.retry = 0 # counts nubmer of retries
-        self.busy_retry = 0  # counts number of retries due to relay busy status
+        ## ## _rawWrite/_rawRead
+        ## self.write_address = 0x0
+        ## self.read_address  = 0x4
+        ## 
+        ## self.relay_address = 0x2
+        ## self.hemt_bias_address = 0x3
+        ## self.a50K_bias_address = 0x4
+        ## self.temperature_address = 0x5
+        ## self.cycle_count_address = 0x6  # used for testing
+        ## self.ps_en_address = 0x7 # PS enable (HEMT: bit 0, 50k: bit 1)
+        ## self.ac_dc_status_address = 0x8 # AC/DC mode status (bit 0: FRN_RLY, bit 1: FRP_RLY)
+        ## self.adc_scale = 3.3/(1024.0 * 5)
+        ## self.temperature_scale = 1/.028 # was 100
+        ## self.temperature_offset =.25
+        ## self.bias_scale = 1.0
+        ## self.max_retries = 5  #number of re-tries waiting for response
+        ## self.retry = 0 # counts nubmer of retries
+        ## self.busy_retry = 0  # counts number of retries due to relay busy status
 
         self.add(pr.RemoteVariable(
             name        = "write",
@@ -65,86 +65,91 @@ class SpiCryo(pr.Device):
             hidden      = True,
         ))
 
-        self.add(pr.LinkVariable(
-            name         = "temperature",
-            description  = "temperature",
-            dependencies = [],
-            linkedGet    = lambda dev, var, read: dev.read_temperature(dev, var, read),
-            typeStr      = "Float64",
-            mode         = "RO",
-        ))
+        ## 6/12/23 - SWH commented out ; someone started trying to
+        ## move cryostat card interface into rogue but not working.
+        ## Good idea, leaving to maybe complete later.
+        ##
+        ## self.add(pr.LinkVariable(
+        ##     name         = "temperature",
+        ##     description  = "temperature",
+        ##     dependencies = [],
+        ##     linkedGet    = lambda dev, var, read: dev.read_temperature(dev, var, read),
+        ##     typeStr      = "Float64",
+        ##     mode         = "RO",
+        ## ))
+        ## 
+        ## self.add(pr.LinkVariable(
+        ##     name         = "50kBias",
+        ##     description  = "temperature",
+        ##     dependencies = [],
+        ##     linkedGet    = lambda dev, var, read: dev.read_50k_bias(dev, var, read),
+        ##     typeStr      = "Float64",
+        ##     mode         = "RO",
+        ## ))
+        ## 
+        ## self.add(pr.LinkVariable(
+        ##     name         = "hemtBias",
+        ##     description  = "hemtBias",
+        ##     dependencies = [],
+        ##     linkedGet    = lambda dev, var, read: dev.read_hemt_bias(dev, var, read),
+        ##     typeStr      = "Float64",
+        ##     mode         = "RO",
+        ## ))
 
-        self.add(pr.LinkVariable(
-            name         = "50kBias",
-            description  = "temperature",
-            dependencies = [],
-            linkedGet    = lambda dev, var, read: dev.read_50k_bias(dev, var, read),
-            typeStr      = "Float64",
-            mode         = "RO",
-        ))
-
-
-
-        self.add(pr.LinkVariable(
-            name         = "hemtBias",
-            description  = "hemtBias",
-            dependencies = [],
-            linkedGet    = lambda dev, var, read: dev.read_hemt_bias(dev, var, read),
-            typeStr      = "Float64",
-            mode         = "RO",
-        ))
-
-
-    def cmd_read(self, data):  # checks for a read bit set in data
-        return ( (data & 0x80000000) != 0)
-
-    def cmd_address(self, data): # returns address data
-        return ((data & 0x7FFF0000) >> 20)
-
-    def cmd_data(self, data):  # returns data
-        return (data & 0xFFFFF)
-
-    def cmd_make(self, read, address, data):
-        return ((read << 31) | ((address << 20) & 0x7FFF00000) | (data & 0xFFFFF))
-
-    def do_read(self, address):
-        #need double write to make sure buffer is updated
-        self._rawWrite(self.write_address, self.cmd_make(1, address, 0))
-        for self.retry in range(0, self.max_retries):
-            self._rawWrite(self.write_address, self.cmd_make(1, address, 0))
-            data = self._rawRead(self.read_address)
-            addrrb = self.cmd_address(data)
-            if (addrrb == address):
-                return (data)
-        return (0)
-
-    @staticmethod
-    def read_temperature(dev, var, read):
-        data = dev.do_read(dev.temperature_address)
-        volts = (data & 0xFFFFF) * dev.adc_scale
-        return round(((volts - dev.temperature_offset) * dev.temperature_scale),2)
-
-    @staticmethod
-    def write_relays(dev, var, value):  # relay is the bit partern to set
-        dev._rawWrite(dev.write_address, dev.cmd_make(0, dev.relay_address, value))
-        time.sleep(0.1)
-        dev._rawWrite(dev.write_address, dev.cmd_make(0, dev.relay_address, value))
-
-    @staticmethod
-    def read_relays(dev, var, read):
-        for dev.busy_retry in range(0, dev.max_retries):
-            data = dev.do_read(dev.relay_address)
-            if ~(data & 0x80000):  # check that not moving
-                return (data & 0x7FFFF)
-                time.sleep(0.1) # wait for relays to move
-        return (80000) # busy flag still set
-
-    @staticmethod
-    def read_hemt_bias(dev, var, read):
-        data = dev.do_read(dev.hemt_bias_address)
-        return round(((data& 0xFFFFF) * dev.bias_scale * dev.adc_scale),6)
-
-    @staticmethod
-    def read_50k_bias(dev, var, read):
-        data = dev.do_read(dev.a50K_bias_address)
-        return round(((data& 0xFFFFF) * dev.bias_scale * dev.adc_scale), 6)
+    ## 6/12/23 - SWH commented out ; someone started trying to
+    ## move cryostat card interface into rogue but not working.
+    ## Good idea, leaving to maybe complete later.
+    ##
+    ## def cmd_read(self, data):  # checks for a read bit set in data
+    ##     return ( (data & 0x80000000) != 0)
+    ## 
+    ## def cmd_address(self, data): # returns address data
+    ##     return ((data & 0x7FFF0000) >> 20)
+    ## 
+    ## def cmd_data(self, data):  # returns data
+    ##     return (data & 0xFFFFF)
+    ## 
+    ## def cmd_make(self, read, address, data):
+    ##     return ((read << 31) | ((address << 20) & 0x7FFF00000) | (data & 0xFFFFF))
+    ## 
+    ## def do_read(self, address):
+    ##     #need double write to make sure buffer is updated
+    ##     self._rawWrite(self.write_address, self.cmd_make(1, address, 0))
+    ##     for self.retry in range(0, self.max_retries):
+    ##         self._rawWrite(self.write_address, self.cmd_make(1, address, 0))
+    ##         data = self._rawRead(self.read_address)
+    ##         addrrb = self.cmd_address(data)
+    ##         if (addrrb == address):
+    ##             return (data)
+    ##     return (0)
+    ## 
+    ## @staticmethod
+    ## def read_temperature(dev, var, read):
+    ##     data = dev.do_read(dev.temperature_address)
+    ##     volts = (data & 0xFFFFF) * dev.adc_scale
+    ##     return round(((volts - dev.temperature_offset) * dev.temperature_scale),2)
+    ## 
+    ## @staticmethod
+    ## def write_relays(dev, var, value):  # relay is the bit partern to set
+    ##     dev._rawWrite(dev.write_address, dev.cmd_make(0, dev.relay_address, value))
+    ##     time.sleep(0.1)
+    ##     dev._rawWrite(dev.write_address, dev.cmd_make(0, dev.relay_address, value))
+    ## 
+    ## @staticmethod
+    ## def read_relays(dev, var, read):
+    ##     for dev.busy_retry in range(0, dev.max_retries):
+    ##         data = dev.do_read(dev.relay_address)
+    ##         if ~(data & 0x80000):  # check that not moving
+    ##             return (data & 0x7FFFF)
+    ##             time.sleep(0.1) # wait for relays to move
+    ##     return (80000) # busy flag still set
+    ## 
+    ## @staticmethod
+    ## def read_hemt_bias(dev, var, read):
+    ##     data = dev.do_read(dev.hemt_bias_address)
+    ##     return round(((data& 0xFFFFF) * dev.bias_scale * dev.adc_scale),6)
+    ## 
+    ## @staticmethod
+    ## def read_50k_bias(dev, var, read):
+    ##     data = dev.do_read(dev.a50K_bias_address)
+    ##     return round(((data& 0xFFFFF) * dev.bias_scale * dev.adc_scale), 6)
