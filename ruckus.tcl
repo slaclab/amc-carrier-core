4c3ac1d37eaeade6844b4de76b08ac0edfd3e692
Larry Ruckman <ruckman@slac.stanford.edu>
2023-09-27 08:35:45 -0700

    updating to latest surf

--- ruckus.tcl
+++ ruckus.tcl
@@ -10,7 +10,7 @@ if { [VersionCheck 2018.3 ] < 0 } {
 if { [info exists ::env(OVERRIDE_SUBMODULE_LOCKS)] != 1 || $::env(OVERRIDE_SUBMODULE_LOCKS) == 0 } {
    if { [SubmoduleCheck {lcls-timing-core} {3.7.7}  "mustBeExact" ] < 0 } {exit -1}
    if { [SubmoduleCheck {ruckus}           {4.8.5}  "mustBeExact" ] < 0 } {exit -1}
-   if { [SubmoduleCheck {surf}             {2.43.0} "mustBeExact" ] < 0 } {exit -1}
+   if { [SubmoduleCheck {surf}             {2.45.1} "mustBeExact" ] < 0 } {exit -1}
 } else {
    puts "\n\n*********************************************************"
    puts "OVERRIDE_SUBMODULE_LOCKS != 0"
