636d9d583ca7c0860f87ebc009efaa8f83f7adac
Larry Ruckman <ruckman@slac.stanford.edu>
2023-11-30 13:01:14 -0800

    updating surf/ruckus submodule locks

--- ruckus.tcl
+++ ruckus.tcl
@@ -9,8 +9,8 @@ if { [VersionCheck 2018.3 ] < 0 } {
 # Check for submodule tagging
 if { [info exists ::env(OVERRIDE_SUBMODULE_LOCKS)] != 1 || $::env(OVERRIDE_SUBMODULE_LOCKS) == 0 } {
    if { [SubmoduleCheck {lcls-timing-core} {3.7.7}  "mustBeExact" ] < 0 } {exit -1}
-   if { [SubmoduleCheck {ruckus}           {4.8.5}  "mustBeExact" ] < 0 } {exit -1}
-   if { [SubmoduleCheck {surf}             {2.45.2} "mustBeExact" ] < 0 } {exit -1}
+   if { [SubmoduleCheck {ruckus}           {4.9.0}  "mustBeExact" ] < 0 } {exit -1}
+   if { [SubmoduleCheck {surf}             {2.45.4} "mustBeExact" ] < 0 } {exit -1}
 } else {
    puts "\n\n*********************************************************"
    puts "OVERRIDE_SUBMODULE_LOCKS != 0"
