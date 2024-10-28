6a88cfb8a47440228f3cad408a6cb9a15e8edffe
mmishra9 <mmishra9@slac.stanford.edu>
2024-10-28 13:24:44 -0700

    added adf5356

--- python/AmcCarrierCore/AppHardware/AmcMicrowaveMux/__init__.py
+++ python/AmcCarrierCore/AppHardware/AmcMicrowaveMux/__init__.py
@@ -2,4 +2,5 @@
 from AmcCarrierCore.AppHardware.AmcMicrowaveMux._amcMicrowaveMuxCtrl import *
 from AmcCarrierCore.AppHardware.AmcMicrowaveMux._amcMicrowaveMuxCore import *
 from AmcCarrierCore.AppHardware.AmcMicrowaveMux._adf5355 import *
+from AmcCarrierCore.AppHardware.AmcMicrowaveMux._adf5356 import *
 from AmcCarrierCore.AppHardware.AmcMicrowaveMux._hmc305 import *
